077d3d3bfefe54dba5661809154bff2a5a0c76d2
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-05-05 05:52:08 +0300

    Update wifire.lua

--- Video Scripts/wifire.lua
+++ Video Scripts/wifire.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "wifire" https://wifire.tv (4/5/21)
+-- видеоскрипт для плейлиста "wifire" https://wifire.tv (5/5/20)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: wifire_pls.lua
@@ -17,17 +17,15 @@
 	if not m_simpleTV.User.wifire then
 		m_simpleTV.User.wifire = {}
 	end
-	local function showError(str)
+	local function showMsg(str)
 		m_simpleTV.OSD.ShowMessageT({text = 'wifire ошибка: ' .. str, showTime = 5000, color = 0xffff1000, id = 'channelName'})
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:87.0) Gecko/20100101 Firefox/87.0')
-		if not session then
-			showError('0')
-		 return
-		end
-	m_simpleTV.Http.SetTimeout(session, 16000)
+	local userAgent = 'Mozilla/5.0 (Linux; U; Android 4.1.1; POV_TV-HDMI-200BT Build/JRO03H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30'
+	local session = m_simpleTV.Http.New(userAgent)
+		if not session then return end
+	m_simpleTV.Http.SetTimeout(session, 8000)
 	local function GetToken()
 		local url = 'https://api.wifire.tv/api/v1/salt/web'
 		local headers = 'Referer: https://wifire.tv/'
@@ -46,82 +44,73 @@
 		m_simpleTV.User.wifire.token = GetToken()
 			if not m_simpleTV.User.wifire.token then
 				m_simpleTV.Http.Close(session)
-				showError('1')
+				showMsg('1')
 			 return
 			end
 	end
-	local retAdr = 'https://api.wifire.tv/proxy/cookies?url=https%3A%2F%2Fsgw.tv.ti.ru%2FstreamingGateway%2FGetLivePlayList%3Fsource%3D'
+	local extOpt = '$OPT:no-ts-trust-pcr$OPT:adaptive-use-access$OPT:http-user-agent=' .. userAgent
+	local retAdr = 'http://api.wifire.tv/proxy/cookies?url=http%3A%2F%2Fsgw.tv.ti.ru%2FstreamingGateway%2FGetLivePlayList%3Fsource%3D'
 			.. inAdr:gsub('https://wifire.tv/', '')
 			.. '%26serviceArea%3DNBN_SA&c='
 			.. m_simpleTV.User.wifire.token
-	local extOpt = '$OPT:adaptive-hls-ignore-discontinuity'
 	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then
-			showError('2')
+			m_simpleTV.User.wifire.token = nil
+			showMsg('2')
 		 return
 		end
-	retAdr:gsub('https://', 'http://')
-	local base = retAdr:match('.+/')
-	local t, i = {}, 1
-		for res, br, res1, adr in answer:gmatch('EXT%-X%-STREAM%-IN([%C]+)[:,]BANDWIDTH=(%d+)([%C]*).-\n(.-)\n') do
-			t[i] = {}
-			br = tonumber(br)
-			br = math.ceil(br / 10000) * 10
-			res = res:match('RESOLUTION=(%d+x%d+)')
-				or res1:match('RESOLUTION=(%d+x%d+)')
-			if res then
-				t[i].Name = res .. ' (' .. br .. ' кбит/с)'
-				res = res:match('x(%d+)')
-				t[i].Id = tonumber(res)
-			else
-				t[i].Name = 'аудио (' .. br .. ' кбит/с)'
-				t[i].Id = 0
+	local t = {}
+		for w in answer:gmatch('EXT%-X%-STREAM%-INF(.-\n.-)\n') do
+			local adr = w:match('\n(.+)')
+			local bw = w:match('BANDWIDTH=(%d+)')
+			local res = w:match('RESOLUTION=%d+x(%d+)')
+			if adr and bw then
+				bw = tonumber(bw)
+				bw = math.ceil(bw / 100000) * 100
+				t[#t + 1] = {}
+				t[#t].Id = bw
+				if res then
+					t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
+				else
+					t[#t].Name = bw .. ' кбит/с'
+				end
+				t[#t].Address = adr:gsub('https', 'http') .. extOpt
 			end
-			if not adr:match('^%s*http') then
-				adr = base .. adr:gsub('^[%s/%.]+', '')
-			end
-			adr = adr:gsub('%-vid%-', '')
-			adr = adr:gsub('^[%c%s]*(.-)[%c%s]*$', '%1')
-			t[i].Address = adr:gsub('https://', 'http://') .. extOpt
-			i = i + 1
 		end
-		if i == 1 then
+		if #t == 0 then
 			m_simpleTV.Control.CurrentAddress = retAdr .. extOpt
 		 return
 		end
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('wifire_qlty') or 5000)
 	table.sort(t, function(a, b) return a.Id < b.Id end)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('wifire_qlty') or 100000000)
+	t[#t + 1] = {}
+	t[#t].Id = 100000000
+	t[#t].Name = '▫ всегда высокое'
+	t[#t].Address = t[#t - 1].Address
+	t[#t + 1] = {}
+	t[#t].Id = 500000000
+	t[#t].Name = '▫ адаптивное'
+	t[#t].Address = retAdr .. extOpt
 	local index = #t
-	if #t > 1 then
-		t[#t + 1] = {}
-		t[#t].Id = 5000
-		t[#t].Name = '▫ всегда высокое'
-		t[#t].Address = t[#t - 1].Address
-		t[#t + 1] = {}
-		t[#t].Id = 10000
-		t[#t].Name = '▫ адаптивное'
-		t[#t].Address = retAdr .. extOpt
-		index = #t
-			for i = 1, #t do
-				if t[i].Id >= lastQuality then
-					index = i
-				 break
-				end
-			end
-		if index > 1 then
-			if t[index].Id > lastQuality then
-				index = index - 1
+		for i = 1, #t do
+			if t[i].Id >= lastQuality then
+				index = i
+			 break
 			end
 		end
-		if m_simpleTV.Control.MainMode == 0 then
-			t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
-			t.ExtParams = {LuaOnOkFunName = 'wifireSaveQuality'}
-			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
+	if index > 1 then
+		if t[index].Id > lastQuality then
+			index = index - 1
 		end
+	end
+	if m_simpleTV.Control.MainMode == 0 then
+		t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
+		t.ExtParams = {LuaOnOkFunName = 'wifireSaveQuality'}
+		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
 	end
 	m_simpleTV.Control.CurrentAddress = t[index].Address
 	function wifireSaveQuality(obj, id)
-		m_simpleTV.Config.SetValue('wifire_qlty', id)
+		m_simpleTV.Config.SetValue('wifire_qlty', tostring(id))
 	end
 -- debug_in_file(m_simpleTV.Control.CurrentAddress .. '\n')
