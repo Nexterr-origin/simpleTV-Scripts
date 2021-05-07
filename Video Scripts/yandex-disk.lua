56648f69b08853b8ab5305e1dc4458d19a2793b2
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-05-07 10:55:30 +0300

    Update yandex-disk.lua

--- Video Scripts/yandex-disk.lua
+++ Video Scripts/yandex-disk.lua
@@ -1,12 +1,14 @@
--- видеоскрипт "Яндекс.Диск" https://disk.yandex.ru (25/3/21)
+-- видеоскрипт "Яндекс.Диск" https://disk.yandex.ru (7/5/21)
 -- Copyright © 2017-2021 Nexter | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## открывает подобные ссылки ##
 -- https://disk.yandex.ru/i/BgqqM3DBhAwAxw
+-- https://disk.yandex.ru/mail?hash=npYAxrOu%2B1hqKJQXU5azg2aSsYX8iSnkGw2WEOk9llESDKrUk0jUzRk2oJr04pVbq%2FJ6bpmRyOJonT3VoXnDag%3D%3D
 -- ##
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('https?://disk%.yandex%.ru') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('https?://disk%.yandex%.') then return end
+	local logo = 'https://disk.yandex.ru/favicon.ico'
 	if m_simpleTV.Control.MainMode == 0 then
-		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'https://disk.yandex.ru/favicon.ico', TypeBackColor = 0, UseLogo = 1, Once = 1})
+		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = logo, TypeBackColor = 0, UseLogo = 1, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAdress = 'Yes'
@@ -14,12 +16,17 @@
 	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:87.0) Gecko/20100101 Firefox/87.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local url = decode64('aHR0cHM6Ly9jbG91ZC1hcGkueWFuZGV4Lm5ldC92MS9kaXNrL3B1YmxpYy9yZXNvdXJjZXMvZG93bmxvYWQ/cHVibGljX2tleT0')
-			.. m_simpleTV.Common.toPercentEncoding(inAdr)
+	inAdr = inAdr:match('hash=([^&]*)') or inAdr
+	inAdr = m_simpleTV.Common.toPercentEncoding(inAdr)
+	local url = decode64('aHR0cHM6Ly9jbG91ZC1hcGkueWFuZGV4Lm5ldC92MS9kaXNrL3B1YmxpYy9yZXNvdXJjZXMvZG93bmxvYWQ/cHVibGljX2tleT0') .. inAdr
 	local rc, answer = m_simpleTV.Http.Request(session, {url = url})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	local retAdr = answer:match('"href":"([^"]+)')
 		if not retAdr then return end
 	m_simpleTV.Control.CurrentAddress = retAdr
+	if m_simpleTV.Control.MainMode == 0 then
+		m_simpleTV.Control.ChangeChannelLogo(logo, m_simpleTV.Control.ChannelID)
+	end
+	m_simpleTV.Control.CurrentTitle_UTF8 = 'Яндекс.Диск'
 -- debug_in_file(retAdr .. '\n')
