fd155b27648744b08766ad36f54e89904586daf0
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-05-19 22:11:32 +0300

    Update vinteratv_pls.lua

--- Scrapers TVSources/vinteratv_pls.lua
+++ Scrapers TVSources/vinteratv_pls.lua
@@ -1,4 +1,4 @@
--- скрапер TVS для загрузки плейлиста "Винтера" с сайта http://www.vintera.tv (7/1/19)
+-- скрапер TVS для загрузки плейлиста "Винтера" http://www.vintera.tv (19/5/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- видоскрипт: vintera.lua
@@ -20,7 +20,6 @@ local filter = {
 	{'БИМ', 'BIM TV'},
 	{'СТВ', 'СвоёТВ (Ставрополь)'},
 	{'Немецкая волна', 'Deutsche Welle'},
-	{'БОЙЦОВСКИЙ ДУХ', 'Бойцовский дух'},
 	{'АТВ-Ставрополь', 'АТВ-Ставрополь (Ставрополь)'},
 	{'Ю.', 'Ю'},
 	{'МТВ', 'МТВ (Волгоград)'},
@@ -56,64 +55,44 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-		local scrap_settings = {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\vinteratv.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 0, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 1, AutoSearch = 1, AutoNumber = 1, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0}}
-	 return scrap_settings
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\vinteratv.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
 	end
-	function GetVersion() return 2,'UTF-8' end
+	function GetVersion()
+	 return 2, 'UTF-8'
+	end
+	local function showMsg(str, color)
+		local t = {text = str, color = color, showTime = 1000 * 10, id = 'channelName'}
+		m_simpleTV.OSD.ShowMessageT(t)
+	end
 	function LoadFromSite()
 		local session = m_simpleTV.Http.New('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 4 Build/LMY48T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.89 Mobile Safari/537.36')
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
-		local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvaW50ZXJuZXR0di54bWw/bGFuZz1ydSZwcm92dHYueG1sJmxhbmc9cnU=')})
-			if rc ~= 200 then m_simpleTV.Http.Close(session) return end
-		answer = answer:gsub('&#x(%x%x%x);', function(h) local i = tonumber(h, 16)
-		return m_simpleTV.Common.UTF16ToUTF8(string.char(i%256, math.floor(i/256), 0, 0)) end)
-		if not m_simpleTV.Common.isUTF8(answer) then
-			answer = m_simpleTV.Common.UTF8ToMultiByte(answer)
-		end
-		local t, i = {}, 1
-		local title, adr
-			for w in answer:gmatch('<track(.-)</track>') do
-				adr = w:match('<location>(.-)</location>')
-				title = w:match('<title>(.-)</title>')
-					if not adr or not title then break end
-				t[i] = {}
-				t[i].Id = i
-				t[i].name = title
-				t[i].address = adr:gsub('&amp;', '&') .. '&tvin'
-				i = i + 1
-			end
-		rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjA1MTcvcHJlbWl1bS9wYWNrYWdlc19ydS54bWw=')})
-		m_simpleTV.Http.Close(session)
-			if rc ~= 200 then return t end
-		if not m_simpleTV.Common.isUTF8(answer) then
-			answer = m_simpleTV.Common.UTF8ToMultiByte(answer)
-		end
-		local t2, i = {}, 1
-			for w in answer:gmatch('<track>(.-)</track>') do
-				adr = w:match('<location>(.-)</location>')
-				title = w:match('<title>(.-)</title>')
-					if not adr or not title then break end
-				t2[i] = {}
-				t2[i].Id = i
-				t2[i].name = title
-				t2[i].address = adr:gsub('amp;', '') .. '&tvin'
-				i = i + 1
-			end
-			for _, v in pairs(t2) do
-				if v.address and v.name then
-					id = v.address or ''
-					if type(t[id]) ~= 'table' then t[id] = {} end
-					t[id].address = v.address
-					t[id].name = v.name
+		htmlEntities = require 'htmlEntities'
+		local urlsTab ={'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvcHJvdnR2LnhtbD9sYW5nPXJ1', 'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvcHJlbWl1bS9wYWNrYWdlc19ydS54bWw', 'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvaW50ZXJuZXR0di54bWw/bGFuZz1ydXM'}
+		local t = {}
+			for i = 1, #urlsTab do
+				local rc, answer = m_simpleTV.Http.Request(session, {url = decode64(urlsTab[i])})
+				if rc == 200 then
+					for w in answer:gmatch('<track(.-)</track>') do
+						local adr = w:match('<location>([^<]+)')
+						local title = w:match('<title>([^<]+)')
+							if adr and adr:match('%.m3u8') and title then
+								t[#t + 1] = {}
+								t[#t].name = htmlEntities.decode(title)
+								t[#t].address = adr:gsub('&amp;', '&') .. '&tvin'
+							end
+					end
 				end
 			end
-		local ret, i = {}, 1, 0
-			for k, v in pairs(t) do
-				ret[i] = v
-				i = i + 1
+		local hash, t0 = {}, {}
+			for _, v in ipairs(t) do
+				if not hash[v.address] then
+					t0[#t0 + 1] = v
+					hash[v.address] = true
+				end
 			end
-	 return ret
+	 return t0
 	end
 	function GetList(UpdateID, m3u_file)
 			if not UpdateID then return end
@@ -121,12 +100,15 @@ local filter = {
 			if not TVSources_var.tmp.source[UpdateID] then return end
 		local Source = TVSources_var.tmp.source[UpdateID]
 		local t_pls = LoadFromSite()
-			if not t_pls then m_simpleTV.OSD.ShowMessageT({text = Source.name .. ' -> ошибка загрузки плейлиста', color = 0xffff6600, showTime = 1000 * 5, id = 'channelName'}) return end
+			if not t_pls then
+				showMsg(Source.name .. ' ошибка загрузки плейлиста', ARGB(255, 255, 102, 0))
+			 return
+			end
 		t_pls = ProcessFilterTableLocal(t_pls)
-		m_simpleTV.OSD.ShowMessageT({text = Source.name .. ' -> ' .. #t_pls, color = 0xff99ff99, showTime = 1000 * 5, id = 'channelName'})
+		showMsg(Source.name .. ' (' .. #t_pls .. ')', ARGB(255, 153, 255, 153))
 		local m3ustr = tvs_core.ProcessFilterTable(UpdateID, Source, t_pls)
 		local handle = io.open(m3u_file, 'w+')
-			if not handle then return nil end
+			if not handle then return end
 		handle:write(m3ustr)
 		handle:close()
 	 return 'ok'
