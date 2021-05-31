cca6c5af851842af783acd2e6d5b510e2e412483
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-06-01 02:42:27 +0300

    Update cloud_mailru.lua

--- Video Scripts/cloud_mailru.lua
+++ Video Scripts/cloud_mailru.lua
@@ -9,12 +9,10 @@
 		 return
 		end
 	local inAdr = m_simpleTV.Control.CurrentAddress
-	local logo = 'https://cdn.4apk.info/img/apk/286.55/icon.png?c=100'
+	local logo = 'https://img.imgsmail.ru/cloud/img/build/release-cloudweb-12166-76-0-0.202105270927/portal-menu/portal-menu__logo.svg'
 	m_simpleTV.OSD.ShowMessageT({text = '', showTime = 1000, id = 'channelName'})
-	if m_simpleTV.Control.MainMode == 0 and not inAdr:match('&Mailru') then
+	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = logo, UseLogo = 1, Once = 1})
-	else
-		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = '', UseLogo = 1, Once = 1})
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
@@ -59,7 +57,7 @@
 			header = header:gsub('%s+', ' ')
 			header = header:gsub('^%s*(.-)%s*$', '%1')
 			local fileEnd = ' (Mailru ' .. os.date('%d.%m.%y') .. ').m3u8'
-			local folder = m_simpleTV.Common.GetMainPath(2) .. m_simpleTV.Common.UTF8ToMultiByte('сохраненые плейлисты') .. '/'
+			local folder = m_simpleTV.Common.GetMainPath(1) .. m_simpleTV.Common.UTF8ToMultiByte('сохраненые плейлисты') .. '/'
 			lfs.mkdir(folder)
 			local folderYT = folder .. 'Mailru/'
 			lfs.mkdir(folderYT)
@@ -73,25 +71,6 @@
 				m_simpleTV.OSD.ShowMessageT({text = 'ошибка сохраненения', color = 0xff9999ff, showTime = 1000 * 5, id = 'channelName'})
 			end
 		end
-	end
-	local function getmailkey()
-			if m_simpleTV.User.Mailru.key then
-			 return m_simpleTV.User.Mailru.key
-			end
-		local session2 = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:89.0) Gecko/20100101 Firefox/89.0')
-			if not session2 then return end
-		m_simpleTV.Http.SetTimeout(session2, 8000)
-		local rc, answer = m_simpleTV.Http.Request(session2, {url = 'https://cloud.mail.ru/api/v2/tokens/download', method = 'post', headers = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8', body = 'api=2&release-cloudweb-12166-76-0-0.202105270927'})
-		m_simpleTV.Http.Close(session2)
-			if rc ~= 200 then
-			 return ''
-			end
-		local token = answer:match('token":"(.-)"')
-			if not token then
-			 return ''
-			end
-		m_simpleTV.User.Mailru.key = '?key=' .. token
-	 return m_simpleTV.User.Mailru.key
 	end
 	local function CloudMailCleanAddress(address)
 		address = address:gsub(' ', '%%20')
@@ -169,15 +148,13 @@
 	local tab = {}
 	local i, n = 1, 1
 	local IndexToplay = 0
-	m_simpleTV.Control.SetTitle(title)
-	m_simpleTV.Control.ChangeChannelLogo(logo, m_simpleTV.Control.ChannelID)
 	local is_find = false
 	local retAdr, name, kind, link, count
 		while true do
 				if not t.folder.list or not t.folder.list[i] then break end
 			name = t.folder.list[i].name
 			kind = t.folder.list[i].kind
-			link = t.folder.list[i].weblink .. getmailkey()
+			link = t.folder.list[i].weblink
 			tab[n] = {}
 			tab[n].Id = n
 			tab[n].Name = name:gsub('_', ' ')
@@ -220,6 +197,7 @@
 		end
 		if not retAdr then
 			if #tab > 0 then
+				m_simpleTV.Control.CurrentTitle_UTF8 = title
 				local _, id = m_simpleTV.OSD.ShowSelect_UTF8(title, 0, tab, 0, 1)
 				id = id or 1
 					if not tab[id] then return end
@@ -241,9 +219,11 @@
 		m_simpleTV.Control.CurrentTitle_UTF8 = title
 		title = m_simpleTV.User.Mailru.Plst[1].Name
 	end
-	if n == 2 then
-		m_simpleTV.Control.CurrentTitle_UTF8 = title
+	if m_simpleTV.Control.MainMode == 0 then
+		local img = tmp:match('"ogImage":%s*"([^"]+)') or logo
+		m_simpleTV.Control.ChangeChannelLogo(img, m_simpleTV.Control.ChannelID)
 	end
+	m_simpleTV.Control.CurrentTitle_UTF8 = title
 	retAdr = retAdr:gsub('&Mailru', '')
 	m_simpleTV.Control.CurrentAddress = retAdr
 	m_simpleTV.OSD.ShowMessageT({text = title, showTime = 1000 * 5, id = 'channelName'})
