d9d3fc6768ba5cebdf73470050cfb9a1cf428266
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-07-28 06:38:26 +0300

    Update kinopoisk.lua

--- Video Scripts/kinopoisk.lua
+++ Video Scripts/kinopoisk.lua
@@ -385,7 +385,7 @@ local tname = {
 			elseif tname[i] == 'Videocdn' then
 				turl[i] = {adr = decode64('aHR0cHM6Ly92aWRlb2Nkbi50di9hcGkvc2hvcnQ/YXBpX3Rva2VuPW9TN1d6dk5meGU0SzhPY3NQanBBSVU2WHUwMVNpMGZtJmtpbm9wb2lza19pZD0') .. kpid, tTitle = 'Большая база фильмов и сериалов', tLogo = logo_k}
 			elseif tname[i] == 'Collaps' then
-				turl[i] = {adr = decode64('aHR0cDovL2FwaS5wbGFjZWhlcmUubGluay9lbWJlZC9rcC8') .. kpid, tTitle = 'Большая база фильмов и сериалов', tLogo = logo_k}
+				turl[i] = {adr = 'http://api.' .. os.time() .. decode64('LnBsYWNlaGVyZS5saW5rL2VtYmVkL2twLw') .. kpid, tTitle = 'Большая база фильмов и сериалов', tLogo = logo_k}
 			elseif tname[i] == 'CDN Movies' then
 				turl[i] = {adr = decode64('aHR0cHM6Ly9jZG5tb3ZpZXMubmV0L2FwaT90b2tlbj1lMmJjYzA5NWEzMDU0NzkyM2ZiMjA4NDRiZGY1ZmM1NCZraW5vcG9pc2tfaWQ9') .. kpid, tTitle = 'Большая база фильмов и сериалов', tLogo = 'https://raw.githubusercontent.com/Nexterr-origin/simpleTV-Images/main/cdnmovie.png'}
 			elseif tname[i] == 'Hdvb' then
