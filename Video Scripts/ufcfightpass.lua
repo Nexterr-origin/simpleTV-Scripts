feeaeccaff04038edd37ede0a19e8981e4653314
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-10-31 12:47:33 +0300

    Update ufcfightpass.lua

--- Video Scripts/ufcfightpass.lua
+++ Video Scripts/ufcfightpass.lua
@@ -3,6 +3,7 @@
 -- логин, пароль установить в 'Password Manager', для id: ufcfightpass
 -- ## открывает подобные ссылки ##
 -- https://ufcfightpass.com/live/129428/fight-pass-247
+-- https://ufcfightpass.com/video/206187/ufc---vs--?playlistId=2939
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://ufcfightpass%.com') then return end
 	local inAdr = m_simpleTV.Control.CurrentAddress
@@ -17,7 +18,8 @@
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
 	local id = inAdr:match('/live/([^/]+)')
-		if not id then
+	local idVod = inAdr:match('/video/([^/]+)')
+		if not id and not idVod then
 			showMsg('неправильная ссылка')
 		 return
 		end
@@ -26,7 +28,7 @@
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local headers = 'Content-Type: application/json\nRealm: dce.ufc\nx-api-key: 857a1e5d-e35e-4fdf-805b-a87b6f8364bf\nAuthorization: Bearer '
-	local url = 'https://dce-frontoffice.imggaming.com/api/v2/'
+	local apiUrl = 'https://dce-frontoffice.imggaming.com/api/v2/'
 	local function showMsg(str)
 		local t = {text = 'UFC Fight Pass: ' .. str, color = ARGB(255, 255, 102, 0), showTime = 1000 * 5, id = 'channelName'}
 		m_simpleTV.OSD.ShowMessageT(t)
@@ -36,21 +38,24 @@
 			if not package.loaded.pm then return end
 		local ret, login, pass = pm.GetTestPassword('ufcfightpass', 'ufcfightpass', true)
 			if not login or not pass or login == '' or pass == '' then return end
-		local url = url .. 'login'
+		local url = apiUrl .. 'login'
 		local headers = headers .. 'null'
 		local body = string.format('{"id":"%s","secret":"%s"}', login, pass)
 		local rc, answer = m_simpleTV.Http.Request(session, {url = url, method = 'post', body = body, headers = headers})
 			if rc ~= 201 then return end
-		local authorisationToken = answer:match('"authorisationToken":"([^"]+)')
-			if not authorisationToken then return end
-	 return authorisationToken
+	 return answer:match('"authorisationToken":"([^"]+)')
 	end
-	local function GetAddress(id, token)
+	local function GetAddress(id, idVod, token)
 		local headers = headers .. token
-		local url = url .. 'stream?sportId=0&propertyId=0&tournamentId=0&displayGeoblockedLive=false&eventId=' .. id
+		local url
+		if id then
+			url = apiUrl .. 'stream?sportId=0&propertyId=0&tournamentId=0&displayGeoblockedLive=false&eventId=' .. id
+		else
+			url = apiUrl .. 'stream/vod/' .. idVod
+		end
 		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
 			if rc ~= 200 then return end
-		local url = answer:match('"playerUrlCallback":"([^"]+)')
+		url = answer:match('"playerUrlCallback":"([^"]+)')
 			if not url then return end
 		rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
 			if rc ~= 200 then return end
@@ -61,9 +66,9 @@
 			showMsg('необходима авторизация')
 		 return
 		end
-	local retAdr = GetAddress(id, authToken)
+	local retAdr = GetAddress(id, idVod, authToken)
 		if not retAdr then
-			showMsg('нет адреса трансляции')
+			showMsg('нет адреса трансляции/видео')
 		 return
 		end
 	local extOpt = '$OPT:http-user-agent=' .. userAgent
