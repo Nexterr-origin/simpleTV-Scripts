815d77ef9c5c951ef5f7be824dcc6b43c3459f21
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-12-01 15:36:41 +0300

    Update tv+hd.lua

--- Video Scripts/tv+hd.lua
+++ Video Scripts/tv+hd.lua
@@ -1,30 +1,28 @@
--- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (24/3/21)
+-- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (1/11/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: tv+hd_pls.lua
 -- расширение дополнения httptimeshift: tvhd-timeshift_ext.lua
 -- ## открывает подобные ссылки ##
 -- https://tv+hd.perviyhd
--- ##
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://tv%+hd%.(%w+)') then return end
-	local inAdr = m_simpleTV.Control.CurrentAddress
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://tv%+hd..+') then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = '', UseLogo = 0, Once = 1})
 	end
+	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:95.0) Gecko/20100101 Firefox/95.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local id = inAdr:match('%.(%w+)')
-	local url
+	local id = inAdr:match('%.([^&]+)')
 	if inAdr:match('&plus=true') then
-		url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2dldHNpZ25lZGR2ci5waHA/c3RyZWFtPQ') .. id
+		inAdr = 'QPtFWZyR3c/AHaw5ic2RGZl52ZpNHdld2L1JnLl5Was52bzVHbwZHduc3d39yL6MHc0RHa'
 	else
-		url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2FwaS9jaGFubmVscy9obHMv') .. id
+		inAdr = 'vMHbo9ycsVmbuFGaj9SawF2L1JnLl5Was52bzVHbwZHduc3d39yL6MHc0RHa'
 	end
-	local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+	local rc, answer = m_simpleTV.Http.Request(session, {url = decode64(string.reverse(inAdr)) .. id})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	answer = answer:gsub('\\/', '/')
