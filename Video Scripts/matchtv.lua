72abd4c2d97e5148bbae6021cefb9a2ddfec0fd1
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-12-25 16:45:49 +0300

    Update matchtv.lua

--- Video Scripts/matchtv.lua
+++ Video Scripts/matchtv.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для сайта https://matchtv.ru (6/1/21)
+-- видеоскрипт для сайта https://matchtv.ru (25/12/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## открывает ссылку ##
 -- https://matchtv.ru/on-air
@@ -6,47 +6,61 @@
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://matchtv%.ru/on%-air') then return end
 	if m_simpleTV.Control.MainMode == 0 then
-		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = '', UseLogo = 0, Once = 1})
+		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = 'https://cdn-assets.matchtv.ru/build/assets/images/logo-matchtv-without-border.6bdcef36.svg', UseLogo = 1, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:83.0) Gecko/20100101 Firefox/83.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:97.0) Gecko/20100101 Firefox/97.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
-		if rc ~= 200 then
-			m_simpleTV.Http.Close(session)
-		 return
-		end
+		if rc ~= 200 then return end
 	local url = answer:match('<iframe.-src="([^"]+)')
 		if not url then return end
-	url = url:gsub('^//', 'http://')
 	rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = 'Referer: ' .. inAdr})
+		if rc ~= 200 then return end
+	url = answer:match('"config=([^"]+)')
+		if not url then return end
+	rc, answer = m_simpleTV.Http.Request(session, {url = url})
+		if rc ~= 200 then return end
+	url = answer:match('<video><!%[CDATA%[([^%]]+)')
+		if not url then return end
+	rc, answer = m_simpleTV.Http.Request(session, {url = url})
+		if rc ~= 200 then return end
+	url = answer:match('<track.-<!%[CDATA%[([^%]]+)')
+		if not url then return end
+	rc, answer = m_simpleTV.Http.Request(session, {url = url})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
-	answer = answer:gsub('%s+', '')
-	answer = answer:match('sourcesWithRes[^%]]+')
-		if not answer then return end
-	local extOpt = ''
-	local t, i = {}, 1
-		for w in answer:gmatch('{.-}') do
-			local res = w:match('res:(%d+)')
-			local name = w:match('label:\'([^\']+)')
-			local adr = w:match('src:\'([^\']+)')
-			if adr and name and res then
+	local i, t = 1, {}
+		for w in string.gmatch(answer,'EXT%-X%-STREAM%-INF(.-%.m3u8.-)\n') do
+			local adr = w:match('\n(.+)')
+			local name = w:match('RESOLUTION=%d+x(%d+)')
+			local br = w:match('BANDWIDTH=(%d+)')
+			if adr and name and br then
+				br = tonumber(br)
+				br = math.ceil(br / 10000) * 10
 				t[i] = {}
-				t[i].Id = 100 - tonumber(res)
-				t[i].Name = name
-				t[i].Address = adr .. extOpt
+				t[i].Id = br
+				t[i].Name = name .. 'p' .. ' (' .. br .. ' кбит/с)'
+				t[i].Address = adr
 				i = i + 1
 			end
 		end
 		if #t == 0 then return end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('matchtv_qlty') or 100)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('matchtv_qlty') or 10000)
 	local index = #t
 	if #t > 1 then
+		t[#t + 1] = {}
+		t[#t].Id = 10000
+		t[#t].Name = '▫ всегда высокое'
+		t[#t].Address = t[#t - 1].Address
+		t[#t + 1] = {}
+		t[#t].Id = 50000
+		t[#t].Name = '▫ адаптивное'
+		t[#t].Address = url
 		index = #t
 			for i = 1, #t do
 				if t[i].Id >= lastQuality then
@@ -62,7 +76,7 @@
 		if m_simpleTV.Control.MainMode == 0 then
 			t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
 			t.ExtParams = {LuaOnOkFunName = 'matchtvSaveQuality'}
-			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
+			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 10000, 32 + 64 + 128)
 		end
 	end
 	m_simpleTV.Control.CurrentAddress = t[index].Address
