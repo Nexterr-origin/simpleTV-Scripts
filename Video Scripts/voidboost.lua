00226ee232b25753c3bd5c52b38d34bec444fefc
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-01-22 00:15:18 +0300

    Update voidboost.lua

--- Video Scripts/voidboost.lua
+++ Video Scripts/voidboost.lua
@@ -1,14 +1,10 @@
--- видеоскрипт для видеобалансера "voidboost" (21/1/22)
+-- видеоскрипт для видеобалансера "voidboost" (22/1/22)
 -- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- модуль: /core/playerjs.lua
 -- ## открывает подобные ссылки ##
 -- https://voidboost.net/serial/019c13f13f6455b96df66f6933ed7bc2/iframe?h=voidboost.net
 -- https://voidboost.net/embed/5928
--- ## прокси ##
-local proxy = ''
--- '' - нет
--- например 'http://proxy-nossl.antizapret.prostovpn.org:29976'
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://voidboost%.net/.+')
 			and not m_simpleTV.Control.CurrentAddress:match('^$voidboost')
@@ -28,9 +24,9 @@ local proxy = ''
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:96.0) Gecko/20100101 Firefox/96.0', proxy, false)
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:96.0) Gecko/20100101 Firefox/96.0')
 		if not session then return end
-	m_simpleTV.Http.SetTimeout(session, 16000)
+	m_simpleTV.Http.SetTimeout(session, 8000)
 	if not m_simpleTV.User then
 		m_simpleTV.User = {}
 	end
@@ -147,16 +143,9 @@ local proxy = ''
 			m_simpleTV.Config.SetValue('voidboost_qlty', t[id].qlty)
 		end
 	end
-	local function play(retAdr, title)
-		if retAdr:match('^$voidboost') then
-			retAdr = GetStream(retAdr)
-				if not retAdr then
-					showError('2')
-				 return
-				end
-		end
-		local subt = voidboostISubt(retAdr)
-		retAdr = voidboostDeSex(retAdr)
+	local function play(answer, title)
+		local subt = voidboostISubt(answer)
+		local retAdr = voidboostDeSex(answer)
 			if not retAdr or retAdr == '' then
 				showError('2.01')
 			 return
@@ -169,6 +158,9 @@ local proxy = ''
 		m_simpleTV.Control.CurrentTitle_UTF8 = title
 		m_simpleTV.OSD.ShowMessageT({text = title, color = 0xff9999ff, showTime = 1000 * 5, id = 'channelName'})
 		retAdr = retAdr .. (subt or '')
+		if inAdr:match('^$voidboost') then
+			retAdr = retAdr .. '$OPT:POSITIONTOCONTINUE=0'
+		end
 		m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
 	end
@@ -183,7 +175,7 @@ local proxy = ''
 		end
 	m_simpleTV.User.voidboost.isVideo = nil
 	m_simpleTV.User.voidboost.titleTab = nil
-	m_simpleTV.User.voidboost.host = inAdr:match('^https?://[^/]+')
+	local host = inAdr:match('^https?://[^/]+')
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 		if rc ~= 200 then
 			showError('4')
@@ -214,7 +206,7 @@ local proxy = ''
 					t[#t + 1] = {}
 					t[#t].Id = #t
 					t[#t].Name = name
-					t[#t].Address = m_simpleTV.User.voidboost.host .. trType .. token .. '/iframe?h=voidboost.net'
+					t[#t].Address = host .. trType .. token .. '/iframe?h=voidboost.net'
 				end
 			end
 		if #t > 1 then
@@ -281,61 +273,62 @@ local proxy = ''
 					showError('6.2')
 				 return
 				end
-		m_simpleTV.User.voidboost.titleTab = t1
-		if m_simpleTV.User.paramScriptForSkin_buttonOptions then
-			t1.ExtButton0 = {ButtonEnable = true, ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonOptions, ButtonScript = 'Qlty_voidboost()'}
-		else
-			t1.ExtButton0 = {ButtonEnable = true, ButtonName = '⚙', ButtonScript = 'Qlty_voidboost()'}
-		end
-		if m_simpleTV.User.paramScriptForSkin_buttonOk then
-			t1.OkButton = {ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonOk}
-		end
-		if m_simpleTV.User.paramScriptForSkin_buttonClose then
-			t1.ExtButton1 = {ButtonEnable = true, ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonClose, ButtonScript = 'OnMultiAddressCancel_voidboost()'}
-		else
-			t1.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'OnMultiAddressCancel_voidboost()'}
-		end
-		t1.ExtParams = {}
-		t1.ExtParams.LuaOnCancelFunName = 'OnMultiAddressCancel_voidboost'
-		t1.ExtParams.LuaOnOkFunName = 'OnMultiAddressOk_voidboost'
-		t1.ExtParams.LuaOnTimeoutFunName = 'OnMultiAddressCancel_voidboost'
-		local pl
-		if #t1 > 1 then
-			pl = 0
-		else
-			pl = 32
-		end
-		title = title .. season_title
-		m_simpleTV.OSD.ShowSelect_UTF8(title, 0, t1, 10000, 2 + 64 + pl)
-		local rc, answer = m_simpleTV.Http.Request(session, {url = t1[1].Address:gsub('$voidboost', '')})
-			if rc ~= 200 then
-				showError('7')
-			 return
+			m_simpleTV.User.voidboost.titleTab = t1
+			if m_simpleTV.User.paramScriptForSkin_buttonOptions then
+				t1.ExtButton0 = {ButtonEnable = true, ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonOptions, ButtonScript = 'Qlty_voidboost()'}
+			else
+				t1.ExtButton0 = {ButtonEnable = true, ButtonName = '⚙', ButtonScript = 'Qlty_voidboost()'}
 			end
-		local subt = voidboostISubt(answer)
-		local retAdr = voidboostDeSex(answer)
-			if not retAdr or retAdr == '' then
-				showError('7.1')
-			 return
+			if m_simpleTV.User.paramScriptForSkin_buttonOk then
+				t1.OkButton = {ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonOk}
 			end
-		retAdr = GetAdr(retAdr)
-			if not retAdr then
-				showError('7.2')
-			 return
+			if m_simpleTV.User.paramScriptForSkin_buttonClose then
+				t1.ExtButton1 = {ButtonEnable = true, ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonClose, ButtonScript = 'OnMultiAddressCancel_voidboost()'}
+			else
+				t1.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'OnMultiAddressCancel_voidboost()'}
 			end
-		m_simpleTV.User.voidboost.DelayedAddress = retAdr .. (subt or '')
-		m_simpleTV.User.voidboost.title = title
-		if #t1 > 1 then
-			inAdr = 'wait'
-			m_simpleTV.User.voidboost.isVideo = false
-		else
-			inAdr = retAdr .. (subt or '')
+			t1.ExtParams = {}
+			t1.ExtParams.PlayMode = 1
+			t1.ExtParams.LuaOnCancelFunName = 'OnMultiAddressCancel_voidboost'
+			t1.ExtParams.LuaOnOkFunName = 'OnMultiAddressOk_voidboost'
+			t1.ExtParams.LuaOnTimeoutFunName = 'OnMultiAddressCancel_voidboost'
+			local pl
+			if #t1 > 1 then
+				pl = 0
+			else
+				pl = 32
+			end
+			title = title .. season_title
+			m_simpleTV.OSD.ShowSelect_UTF8(title, 0, t1, 10000, 2 + 64 + pl)
+			local rc, answer = m_simpleTV.Http.Request(session, {url = t1[1].Address:gsub('$voidboost', '')})
+				if rc ~= 200 then
+					showError('7')
+				 return
+				end
+			local subt = voidboostISubt(answer)
+			local retAdr = voidboostDeSex(answer)
+				if not retAdr or retAdr == '' then
+					showError('7.1')
+				 return
+				end
+			retAdr = GetAdr(retAdr)
+				if not retAdr then
+					showError('7.2')
+				 return
+				end
+			m_simpleTV.User.voidboost.DelayedAddress = retAdr .. (subt or '') .. '$OPT:POSITIONTOCONTINUE=0'
+			m_simpleTV.User.voidboost.title = title
+			if #t1 > 1 then
+				retAdr = 'wait'
+				m_simpleTV.User.voidboost.isVideo = false
+			else
+				retAdr = retAdr .. (subt or '') .. '$OPT:POSITIONTOCONTINUE=0'
+			end
+			m_simpleTV.Control.CurrentTitle_UTF8 = title
+			m_simpleTV.OSD.ShowMessageT({text = title .. ' - ' .. m_simpleTV.User.voidboost.titleTab[1].Name, color = 0xff9999ff, showTime = 1000 * 5, id = 'channelName'})
+			m_simpleTV.Control.CurrentAddress = retAdr
+		 return
 		end
-		m_simpleTV.Control.CurrentTitle_UTF8 = title
-		m_simpleTV.OSD.ShowMessageT({text = title .. ' - ' .. m_simpleTV.User.voidboost.titleTab[1].Name, color = 0xff9999ff, showTime = 1000 * 5, id = 'channelName'})
-		m_simpleTV.Control.CurrentAddress = inAdr
-	 return
-	end
 	else
 		local t = {}
 		t[1] = {}
