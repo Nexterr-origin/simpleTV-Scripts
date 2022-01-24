24952565c5752870cce6a23df82f33656f79e65a
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-01-24 19:32:24 +0300

    Update ufcfightpass.lua

--- Video Scripts/ufcfightpass.lua
+++ Video Scripts/ufcfightpass.lua
@@ -1,16 +1,25 @@
--- видеоскрипт для сайта https://ufcfightpass.com (31/10/21)
--- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для сайта https://ufcfightpass.com (24/1/22)
+-- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- логин, пароль установить в 'Password Manager', для id: ufcfightpass
 -- ## открывает подобные ссылки ##
 -- https://ufcfightpass.com/live/129428/fight-pass-247
 -- https://ufcfightpass.com/video/206187/ufc---vs--?playlistId=2939
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://ufcfightpass%.com') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://ufcfightpass%.com')
+			and not m_simpleTV.Control.CurrentAddress:match('PARAMS=ufcfightpass')
+		then
+		 return
+		end
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	local logo = 'https://static.diceplatform.com/prod/AUTOx100/dce.ufc/settings/UFC-FIGHT-PASS-Vertical_White.98nci.png'
 	if m_simpleTV.Control.MainMode == 0 then
-		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = logo, UseLogo = 1, Once = 1})
+		local pic = logo
+		if inAdr:match('PARAMS=ufcfightpass') then
+			pic = ''
+		end
+		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = pic, UseLogo = 1, Once = 1})
 	end
+	inAdr = inAdr:match('PARAMS=ufcfightpass([^$]+)') or inAdr
 	local function showMsg(str)
 		local t = {text = 'UFC Fight Pass: ' .. str, color = ARGB(255, 255, 102, 0), showTime = 1000 * 5, id = 'channelName'}
 		m_simpleTV.OSD.ShowMessageT(t)
@@ -23,7 +32,7 @@
 			showMsg('неправильная ссылка')
 		 return
 		end
-	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:94.0) Gecko/20100101 Firefox/94.0'
+	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:97.0) Gecko/20100101 Firefox/97.0'
 	local session = m_simpleTV.Http.New(userAgent)
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
@@ -44,11 +53,11 @@
 	local function GetAddress(id, id_vod, token, headers, apiUrl)
 		local url
 		if id then
-			url = apiUrl .. 'stream?sportId=0&propertyId=0&tournamentId=0&displayGeoblockedLive=false&eventId=' .. id
+			url = '?eventId=' .. id
 		else
-			url = apiUrl .. 'stream/vod/' .. id_vod
+			url = '/vod/' .. id_vod
 		end
-		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers .. token})
+		local rc, answer = m_simpleTV.Http.Request(session, {url = apiUrl .. 'stream' .. url, headers = headers .. token})
 			if rc ~= 200 then return end
 		url = answer:match('"playerUrlCallback":"([^"]+)')
 			if not url then return end
@@ -67,14 +76,75 @@
 		 return
 		end
 	title = title or 'UFC Fight Pass'
-	pic = pic or logo
 	if m_simpleTV.Control.MainMode == 0 then
+		pic = pic or logo
 		m_simpleTV.Control.ChangeChannelName(title, m_simpleTV.Control.ChannelID, false)
 		pic = pic:gsub('/original/', '/346x346/')
 		m_simpleTV.Control.ChangeChannelLogo(pic, m_simpleTV.Control.ChannelID)
 	end
 	m_simpleTV.Control.CurrentTitle_UTF8 = title
-	local extOpt = '$OPT:http-user-agent=' .. userAgent
-	retAdr = retAdr .. extOpt
-	m_simpleTV.Control.CurrentAddress = retAdr
--- debug_in_file(retAdr .. '\n')
+	local extOpt = '$OPT:INT-SCRIPT-PARAMS=ufcfightpass' .. inAdr .. '$OPT:adaptive-use-avdemux$OPT:http-user-agent=' .. userAgent
+	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
+		if rc ~= 200 then
+			m_simpleTV.Control.CurrentAddress = retAdr .. extOpt
+		 return
+		end
+	local t0 = {}
+		for w in string.gmatch(answer,'EXT%-X%-STREAM%-INF:BANDWIDTH.-\n') do
+			local name = w:match('RESOLUTION=%d+x(%d+)')
+			local br = w:match('BANDWIDTH=(%d+)')
+			if name and br then
+				t0[#t0 +1] = {}
+				br = tonumber(br)
+				br = math.ceil(br / 1000)
+				t0[#t0].Address = retAdr .. '$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=' .. br .. extOpt
+				t0[#t0].Id = br
+				t0[#t0].Name = name .. 'p' .. ' (' .. br .. ' кбит/с)'
+			end
+		end
+		if #t0 == 0 then
+			m_simpleTV.Control.CurrentAddress = retAdr .. extOpt
+		 return
+		end
+	table.sort(t0, function(a, b) return a.Id < b.Id end)
+	local hash, t = {}, {}
+		for i = 1, #t0 do
+			if not hash[t0[i].Id] then
+				t[#t + 1] = t0[i]
+				hash[t0[i].Id] = true
+			end
+		end
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('ufcfightpass_qlty') or 20000)
+	local index = #t
+	if #t > 1 then
+		t[#t + 1] = {}
+		t[#t].Id = 20000
+		t[#t].Name = '▫ всегда высокое'
+		t[#t].Address = t[#t - 1].Address
+		t[#t + 1] = {}
+		t[#t].Id = 50000
+		t[#t].Name = '▫ адаптивное'
+		t[#t].Address = retAdr .. extOpt
+		index = #t
+			for i = 1, #t do
+				if t[i].Id >= lastQuality then
+					index = i
+				 break
+				end
+			end
+		if index > 1 then
+			if t[index].Id > lastQuality then
+				index = index - 1
+			end
+		end
+		if m_simpleTV.Control.MainMode == 0 then
+			t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
+			t.ExtParams = {LuaOnOkFunName = 'ufcfightpassSaveQuality'}
+			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 10000, 32 + 64 + 128)
+		end
+	end
+	m_simpleTV.Control.CurrentAddress = t[index].Address
+	function ufcfightpassSaveQuality(obj, id)
+		m_simpleTV.Config.SetValue('ufcfightpass_qlty', id)
+	end
+-- debug_in_file(t[index].Address .. '\n')
