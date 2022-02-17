6c8958300c05f510df9b9a3448b2ba9855d9697a
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-02-17 14:31:21 +0300

    Update domatv.lua

--- Video Scripts/domatv.lua
+++ Video Scripts/domatv.lua
@@ -1,11 +1,10 @@
--- видеоскрипт для плейлиста "ДомаТвНет" http://tv.domatv.net (14/10/20)
--- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для плейлиста "ДомаТвНет" http://tv.domatv.net (17/2/22)
+-- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: domatv_pls.lua
 -- модуль: /core/playerjs.lua
 -- ## открывает подобные ссылки ##
--- http://tv.domatv.net/107-bbc-world-news.html
--- ##
+-- http://tv.domatv.net/214-kino-tv.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://[%w%.]*domatv%.net') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -18,7 +17,7 @@
 	local function showError(str)
 		m_simpleTV.OSD.ShowMessageT({text = 'domatv ошибка: ' .. str, showTime = 1000 * 5, color = 0xffff6600, id = 'channelName'})
 	end
-	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:82.0) Gecko/20100101 Firefox/82.0'
+	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:97.0) Gecko/20100101 Firefox/97.0'
 	local session = m_simpleTV.Http.New(userAgent)
 		if not session then
 			showError('0')
@@ -51,10 +50,12 @@
 			showError('4')
 		 return
 		end
-	local v1 = answer:match('firstIpProtect=\'([^\']+)') or ''
-	local v2 = answer:match('secondIpProtect=\'([^\']+)') or ''
+	local v1 = answer:match('varhelloBoyzzzOne=atob%(\'([^\']+)') or ''
+	local v2 = answer:match('varhelloBoyzzzTwo=atob%(\'([^\']+)') or ''
+	v1 = decode64(v1)
+	v2 = decode64(v2)
 	local v3 = answer:match('portProtect=\'([^\']+)') or ''
-	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3)
+	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3):gsub('\n', '')
 	retAdr = retAdr .. '$OPT:http-referrer=' .. inAdr .. '$OPT:http-user-agent=' .. userAgent
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr, m_simpleTV.Common.GetMainPath(2) .. 'domatv.txt', true)
