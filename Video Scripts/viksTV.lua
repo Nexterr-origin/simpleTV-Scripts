5766ff6648b4680ebcb3b881bacdc9d668f8292b
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-02-18 00:36:06 +0300

    Update viksTV.lua

--- Video Scripts/viksTV.lua
+++ Video Scripts/viksTV.lua
@@ -1,10 +1,9 @@
--- видеоскрипт для плейлиста "viksTV" http://online.viks.tv (28/9/20)
--- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для плейлиста "viksTV" http://online.viks.tv (18/2/22)
+-- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: viksTV_pls.lua
 -- ## открывает подобные ссылки ##
--- http://online.viks.tv/602-mir-basketbola.html
--- ##
+-- http://online.viks.tv/421-1000-russkoe-kino.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://online%.viks%.tv') then return end
 	local inAdr = m_simpleTV.Control.CurrentAddress
@@ -13,7 +12,7 @@
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:82.0) Gecko/20100101 Firefox/82.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:98.0) Gecko/20100101 Firefox/98.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
@@ -40,8 +39,12 @@
 	 return decode64(url)
 	end
 	retAdr = DeCipher(retAdr)
-	local v1 = answer:match('firstIpProtect=\'([^\']+)') or ''
-	local v2 = answer:match('secondIpProtect=\'([^\']+)') or ''
+	local v1 = answer:match('varhelloBoyzzzOne=atob%(\'([^\']+)') or ''
+	local v2 = answer:match('varhelloBoyzzzTwo=atob%(\'([^\']+)') or ''
+	v1 = decode64(v1)
+	v2 = decode64(v2)
+	local v3 = answer:match('portProtect=\'([^\']+)') or ''
+	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3):gsub('\n', '')
 	local v3 = answer:match('portProtect=\'([^\']+)') or ''
 	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3)
 	retAdr = retAdr .. '$OPT:http-referrer=' .. inAdr
