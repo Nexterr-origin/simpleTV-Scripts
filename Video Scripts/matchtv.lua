e26be03b0e31b3b8b09fdb62bce6453cefd560da
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-04-26 10:14:18 +0300

    Update matchtv.lua

--- Video Scripts/matchtv.lua
+++ Video Scripts/matchtv.lua
@@ -1,8 +1,7 @@
--- видеоскрипт для сайта https://matchtv.ru (6/1/21)
--- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для сайта https://matchtv.ru (26/4/22)
+-- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## открывает ссылку ##
 -- https://matchtv.ru/on-air
--- ##
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://matchtv%.ru/on%-air') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -11,7 +10,7 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:83.0) Gecko/20100101 Firefox/83.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:99.0) Gecko/20100101 Firefox/99.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
@@ -19,7 +18,7 @@
 			m_simpleTV.Http.Close(session)
 		 return
 		end
-	local url = answer:match('<iframe.-src="([^"]+)')
+	local url = answer:match('<div class="video%-player.-src="([^"]+)')
 		if not url then return end
 	url = url:gsub('^//', 'http://')
 	rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = 'Referer: ' .. inAdr})
