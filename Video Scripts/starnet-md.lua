38532b701ebdc8dfa1221c6b4225750445babe3f
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-10-29 15:13:15 +0300

    Update starnet-md.lua

--- Video Scripts/starnet-md.lua
+++ Video Scripts/starnet-md.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "StarNet" https://www.starnet.md (21/7/22)
+-- видеоскрипт для плейлиста "StarNet" https://www.starnet.md (29/10/22)
 -- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: starnet-md_pls.lua
@@ -31,13 +31,10 @@
 		for w in string.gmatch(answer,'EXT%-X%-STREAM%-INF(.-%.m3u8.-)\n') do
 			local adr = w:match('\n(.+)')
 			local name = w:match('RESOLUTION=%d+x(%d+)')
-			local br = w:match(',BANDWIDTH=(%d+)')
-			if adr and name and br then
-				br = tonumber(br)
-				br = math.floor(br / 100000) * 100
+			if adr and name then
 				t[#t + 1] = {}
-				t[#t].Id = br
-				t[#t].Name = name .. 'p' .. ' (' .. br .. ' кбит/с)'
+				t[#t].Id = tonumber(name)
+				t[#t].Name = name .. 'p'
 				t[#t].Address = base .. adr:gsub('/[^.]+', '/index')
 			end
 		end
@@ -46,9 +43,9 @@
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('starnet_md_qlty') or 5000)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('starnet_md_qlty') or 10000)
 	t[#t + 1] = {}
-	t[#t].Id = 5000
+	t[#t].Id = 10000
 	t[#t].Name = '▫ всегда высокое'
 	t[#t].Address = t[#t - 1].Address
 	local index = #t
@@ -66,7 +63,7 @@
 	if m_simpleTV.Control.MainMode == 0 then
 		t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
 		t.ExtParams = {LuaOnOkFunName = 'starnetmdSaveQuality'}
-		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
+		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128 + 8)
 	end
 	m_simpleTV.Control.CurrentAddress = t[index].Address
 	function starnetmdSaveQuality(obj, id)
