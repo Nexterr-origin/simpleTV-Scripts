726739c607d18bcdce36df8360eaf36fcacba045
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-12-03 12:41:14 +0300

    Update vimeo.lua

--- Video Scripts/vimeo.lua
+++ Video Scripts/vimeo.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для сайта https://vimeo.com/watch (30/7/22)
+-- видеоскрипт для сайта https://vimeo.com/watch (3/12/22)
 -- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## открывает подобные ссылки ##
 -- https://vimeo.com/channels/musicvideoland/368152561
@@ -9,6 +9,7 @@
 -- https://vimeo.com/27945056
 -- https://vimeo.com/showcase/3717822/video/329792082
 -- https://vimeo.com/718108412
+-- https://vimeo.com/771846252
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://[%a%.]*vimeo%.com/.+') then return end
 	local inAdr = m_simpleTV.Control.CurrentAddress
@@ -28,7 +29,7 @@
 			showError('not found \'id\' in url')
 		 return
 		end
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:103.0) Gecko/20100101 Firefox/103.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	function vimeoSaveQuality(obj, id)
@@ -74,11 +75,23 @@
 	answer = answer:gsub('%[%]', ' ')
 	require 'json'
 	local tab = json.decode(answer)
+	local retAdr
 		if not tab
-			or not tab.video
-			or not tab.request
-			or not tab.request.files
+			and not tab.video
+			and not tab.request
+			and not tab.request.files
+			and not tab.request.files.hls
+			and not tab.request.files.hls.cdns
 		then
+			showError('видео не найдено')
+		 return
+		end
+	if tab.request.files.hls.cdns.akamai_live then
+		retAdr = tab.request.files.hls.cdns.akamai_live.url
+	elseif tab.request.files.hls.cdns.akfire_interconnect_quic then
+		retAdr = tab.request.files.hls.cdns.akfire_interconnect_quic.avc_url
+	end
+		if not retAdr then
 			showError('3')
 		 return
 		end
@@ -89,7 +102,7 @@
 			title = addTitle
 		else
 			if m_simpleTV.Control.MainMode == 0 then
-				title = unescape3(title)
+				title = unescape3(title):gsub('\\"', '"')
 				answer = answer:gsub('\\"', '"')
 				m_simpleTV.Control.ChangeChannelName(title, m_simpleTV.Control.ChannelID, false)
 				local thumbs
@@ -102,72 +115,41 @@
 			title = addTitle .. ' - ' .. title
 		end
 	end
-	local t = {}
-	if tab.request.files.progressive and tab.request.files.progressive ~= '' then
-		local i = 1
-			while true do
-					if not tab.request.files.progressive[i] then break end
-				t[i] = {}
-				t[i].Id = tab.request.files.progressive[i].height
-				t[i].Name = tab.request.files.progressive[i].quality
-				t[i].Address = tab.request.files.progressive[i].url:gsub('%?.-$', '') .. '$OPT:NO-STIMESHIFT'
-				i = i + 1
-			end
-	else
-		if tab.request.files.hls and tab.request.files.hls.cdns then
-			local url
-			if tab.request.files.hls.cdns.akamai_live and tab.request.files.hls.cdns.akamai_live.json_url then
-				url = tab.request.files.hls.cdns.akamai_live.json_url
-				rc, answer = m_simpleTV.Http.Request(session, {url = url})
-					if rc ~= 200 then
-						showError('4')
-					 return
-					end
-				url = answer:match('"url":"([^"]+)')
-					if not url then
-						showError('4.1')
-					 return
-					end
-			elseif tab.request.files.hls.cdns.akfire_interconnect_quic and tab.request.files.hls.cdns.akfire_interconnect_quic.url then
-			url = tab.request.files.hls.cdns.akfire_interconnect_quic.url
-			end
-				rc, answer = m_simpleTV.Http.Request(session, {url = url})
-					if rc ~= 200 then
-						showError('4.21')
-					 return
-					end
-				local base = url:match('.+/')
-					for w in answer:gmatch('EXT%-X%-STREAM%-INF.-\n.-\n') do
-						local adr = w:match('\n(.-)\n')
-						local name = w:match('RESOLUTION=%d+x(%d+)')
-						if adr and name then
-							if not adr:match('^http') then
-								if adr:match('^%.%./') then
-									base = url:match('.+/video/') or ''
-								end
-								adr = base .. adr:gsub('^[%./]+', '')
-							end
-							t[#t + 1] = {}
-							t[#t].Id = tonumber(name)
-							t[#t].Name = name .. 'p'
-							t[#t].Address = adr
-						end
-					end
-		end
-	end
+	rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
 	m_simpleTV.Http.Close(session)
+		if rc ~= 200 then
+			showError('нет видео')
+		 return
+		end
+	local t = {}
+	local extOpt = '$OPT:adaptive-use-avdemux'
+		for w in answer:gmatch('EXT%-X%-STREAM%-INF.-\n') do
+			local bw = w:match('[^%-]BANDWIDTH=(%d+)')
+			local res = w:match('RESOLUTION=%d+x(%d+)')
+			if bw and res then
+				bw = math.ceil(bw / 100000) * 100
+					t[#t + 1] = {}
+					t[#t].Id = bw
+					t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
+					t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', retAdr, bw, extOpt)
+			end
+		end
 		if #t == 0 then
 			showError('5')
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('vimeo_qlty') or 5000)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('vimeo_qlty') or 20000)
 	local index = #t
 	if #t > 1 then
 		t[#t + 1] = {}
-		t[#t].Id = 5000
+		t[#t].Id = 20000
 		t[#t].Name = '▫ всегда высокое'
 		t[#t].Address = t[#t - 1].Address
+		t[#t + 1] = {}
+		t[#t].Id = 50000
+		t[#t].Name = '▫ адаптивное'
+		t[#t].Address = retAdr .. extOpt
 		index = #t
 			for i = 1, #t do
 				if t[i].Id >= lastQuality then
