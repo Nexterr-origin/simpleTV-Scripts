a94381624d7ac27b6a6a26b902e7d21a306738cc
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-12-20 05:20:06 +0300

    Update mediavitrina.lua

--- Video Scripts/mediavitrina.lua
+++ Video Scripts/mediavitrina.lua
@@ -1,12 +1,17 @@
--- видеоскрипт для плейлиста "Витрина ТВ" https://www.vitrina.tv (19/12/22)
+-- видеоскрипт для плейлиста "Витрина ТВ" https://www.vitrina.tv (20/12/22)
 -- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: mediavitrina_pls.lua
 -- ## открывает подобные ссылки ##
 -- https://player.mediavitrina.ru/zvezda/tvzvezda/vitrinatv_web/player.html
--- ##
+-- https://media.mediavitrina.ru/api/v2/ntv/playlist/ntv_msk_as_array.json
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://player%.mediavitrina%.ru') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://%a+%.mediavitrina%.ru') then return end
+		if m_simpleTV.Control.CurrentAddress:match('%.m3u8')
+			or m_simpleTV.Control.CurrentAddress:match('%.mpd')
+		then
+		 return
+		end
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
@@ -71,18 +76,20 @@
 			end
 	 return t
 	end
-	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
-		if rc ~= 200 then
-			m_simpleTV.Http.Close(session)
-			showErr(1)
-		 return
-		end
-	local url = answer:match('http[^\'"<>]+as_array%.json')
-		if not url then
-			m_simpleTV.Http.Close(session)
-			showErr(1.1)
-		 return
-		end
+	if not inAdr:match('as_array%.json') then
+		local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
+			if rc ~= 200 then
+				m_simpleTV.Http.Close(session)
+				showErr(1)
+			 return
+			end
+		inAdr = answer:match('http[^\'"<>]+as_array%.json')
+			if not inAdr then
+				m_simpleTV.Http.Close(session)
+				showErr(1.1)
+			 return
+			end
+	end
 	local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly9tZWRpYS5tZWRpYXZpdHJpbmEucnUvZ2V0X3Rva2Vu')})
 		if rc ~= 200 then
 			m_simpleTV.Http.Close(session)
@@ -94,9 +101,9 @@
 			showErr(3)
 		 return
 		end
-	url = url .. '?token=' .. token
-	url = url:gsub('\\/', '/'):gsub('/v3/', '/v1/')
-	rc, answer = m_simpleTV.Http.Request(session, {url = url})
+	inAdr = inAdr .. '?token=' .. token
+	inAdr = inAdr:gsub('\\/', '/'):gsub('/v3/', '/v1/')
+	rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 		if rc ~= 200 then
 			m_simpleTV.Http.Close(session)
 			showErr(4)
@@ -119,15 +126,15 @@
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('mediavitrina_qlty') or 5000000)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('mediavitrina_qlty') or 30000)
 	t[#t + 1] = {}
-	t[#t].Id = 5000000
+	t[#t].Id = 30000
 	t[#t].Name = '▫ всегда высокое'
 	t[#t].Address = t[#t - 1].Address
 	t[#t + 1] = {}
-	t[#t].Id = 100000000
+	t[#t].Id = 50000
 	t[#t].Name = '▫ адаптивное'
-	t[#t].Address = retAdr
+	t[#t].Address = retAdr .. exOpt
 	local index = #t
 		for i = 1, #t do
 			if t[i].Id >= lastQuality then
@@ -143,10 +150,10 @@
 	if m_simpleTV.Control.MainMode == 0 then
 		t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
 		t.ExtParams = {LuaOnOkFunName = 'mediavitrinaSaveQuality'}
-		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
+		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128 + 8)
 	end
 	m_simpleTV.Control.CurrentAddress = t[index].Address
 	function mediavitrinaSaveQuality(obj, id)
-		m_simpleTV.Config.SetValue('mediavitrina_qlty', tostring(id))
+		m_simpleTV.Config.SetValue('mediavitrina_qlty', id)
 	end
 -- debug_in_file(t[index].Address .. '\n')
