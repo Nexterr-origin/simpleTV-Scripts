cf338dc9b16c64d6e6a68c29edffd345706b640d
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-05 14:48:35 +0300

    Update limeHD.lua

--- Video Scripts/limeHD.lua
+++ Video Scripts/limeHD.lua
@@ -1,10 +1,10 @@
--- видеоскрипт для плейлиста "LimeHD" https://limehd.tv (4/1/23)
+-- видеоскрипт для плейлиста "LimeHD" https://limehd.tv (5/1/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: LimeHD_pls.lua
 -- расширение дополнения httptimeshift: limehd-timeshift_ext.lua
 -- ## открывает подобные ссылки ##
--- https://infolink/10352
+-- https://infolink/1
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://infolink/%d') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -17,71 +17,20 @@
 	if not m_simpleTV.User.infolink then
 		m_simpleTV.User.infolink = {}
 	end
-	m_simpleTV.User.infolink.catchup = nil
 	m_simpleTV.User.infolink.url_archive = nil
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local function GetPlst()
-		local session = m_simpleTV.Http.New(decode64('eyJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJhcHAiOiJjb20uaW5mb2xpbmsubGltZWlwdHYiLCJ2ZXJzaW9uX25hbWUiOiIzLjMuMyIsInZlcnNpb25fY29kZSI6IjI1NiIsInNkayI6IjI5IiwibmFtZSI6InNka19waG9uZV94ODZfNjQrQW5kcm9pZCBTREsgYnVpbHQgZm9yIHg4Nl82NCIsImRldmljZV9pZCI6IjAwMEEwMDBBMDAwQTAwMEEifQ'))
-			if not session then return end
-		m_simpleTV.Http.SetTimeout(session, 12000)
-		local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly9wbC5pcHR2MjAyMS5jb20vYXBpL3YxL3BsYXlsaXN0') .. '?t=' .. os.time(), method = 'post', body = '"tz":"3"', headers = 'X-Token:'})
-		m_simpleTV.Http.Close(session)
-			if rc ~= 200 then return end
-			if not answer:match('^{') then return end
-		answer = answer:gsub('%[%]', '""')
-		require 'json'
-		local tab = json.decode(answer)
-			if not tab then return end
-		local t, i = {}, 1
-		local adr
-		local j = 1
-			while true do
-					if not tab.channels[j] then break end
-				adr = tab.channels[j].url
-				if adr and adr ~= '' then
-					t[i] = {}
-					t[i].ServerId = tab.channels[j].id
-					t[i].url_archive = tab.channels[j].url_archive
-					t[i].day_archive = tab.channels[j].day_archive
-					t[i].with_archive = tab.channels[j].with_archive
-					t[i].videoUrl = adr:gsub('^http://', 'https://')
-					i = i + 1
-				end
-				j = j + 1
-			end
-			if i == 1 then return end
-	 return t
-	end
-	if not m_simpleTV.User.infolink.plst then
-		local plst = GetPlst()
-			if not plst then return end
-		m_simpleTV.User.infolink.plst = plst
-	end
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
+		if not session then return end
+	m_simpleTV.Http.SetTimeout(session, 8000)
 	local id = inAdr:match('%d+')
-	local retAdr, url_archive, day_archive
-		for i = 1, #m_simpleTV.User.infolink.plst do
-			if tonumber(id) == tonumber(m_simpleTV.User.infolink.plst[i].ServerId) then
-				retAdr = m_simpleTV.User.infolink.plst[i].videoUrl
-				if m_simpleTV.User.infolink.plst[i].with_archive == true then
-					day_archive = m_simpleTV.User.infolink.plst[i].day_archive
-					url_archive = m_simpleTV.User.infolink.plst[i].url_archive
-					if url_archive
-						and url_archive ~= ''
-						and day_archive
-						and day_archive > 0
-					then
-						m_simpleTV.User.infolink.catchup = 'catchup="append" catchup-days="' .. day_archive
-						m_simpleTV.User.infolink.url_archive = url_archive
-					end
-				end
-			 break
-			end
-		end
-		if not retAdr then
-			m_simpleTV.User.infolink = nil
-		 return
-		end
+	local url = 'https://api.iptv2021.com/v1/streams/' .. id
+	local headers = decode64('WC1BY2Nlc3MtS2V5OiAxMGFhMDkxMTQ1ODhhNWY3NTBlYWVkNWU5ZGU1MzcwNGM4NThlMTQ0')
+	local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
+		if rc ~= 200 then return end
+	local retAdr = answer:match('"playlist_url":"([^"]+)')
+		if not retAdr then return end
+	m_simpleTV.User.infolink.url_archive = answer:match('"archive_url":"([^"]+)')
 	local extOpt = '$OPT:adaptive-logic=highest$OPT:no-spu$OPT:adaptive-use-avdemux'
 	retAdr = retAdr .. extOpt
 	m_simpleTV.Control.CurrentAddress = retAdr
