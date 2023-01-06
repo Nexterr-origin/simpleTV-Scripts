fc4e3b019ed01c2a3e0d3d35ec5d6e789d5c461e
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-06 12:15:48 +0300

    Update limeHD_pls.lua

--- Scrapers TVSources/limeHD_pls.lua
+++ Scrapers TVSources/limeHD_pls.lua
@@ -1,4 +1,4 @@
--- скрапер TVS для загрузки плейлиста "LimeHD" https://limehd.tv (5/1/23)
+-- скрапер TVS для загрузки плейлиста "LimeHD" https://limehd.tv (6/1/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- видоскрипт: limeHD.lua
@@ -23,7 +23,7 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\limehd.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\limehd.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, show_progress = 0, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
 	end
 	function GetVersion()
 	 return 2, 'UTF-8'
@@ -32,7 +32,7 @@ local filter = {
 		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
-		local url = 'https://api.iptv2021.com/v1/channels/by_group/11?locale=ru'
+		local url = decode64('aHR0cHM6Ly9hcGkuaXB0djIwMjEuY29tL3YxL2NoYW5uZWxzL2J5X2dyb3VwLzExP2xvY2FsZT1ydQ')
 		local headers = decode64('WC1BY2Nlc3MtS2V5OiAxMGFhMDkxMTQ1ODhhNWY3NTBlYWVkNWU5ZGU1MzcwNGM4NThlMTQ0')
 		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
 			if rc ~= 200 then return end
@@ -46,12 +46,18 @@ local filter = {
 			end
 		local t, i = {}, 1
 			while tab.data[i] do
-				t[#t + 1] = {}
-				t[#t].name = tab.data[i].attributes.name
-				t[#t].address = 'https://infolink/' .. tab.data[i].attributes.streams[1].id
-				t[#t].logo = tab.data[i].attributes.image_url
-				local archive_minutes = tab.data[i].attributes.streams[1].archive_hours * 60
-				t[#t].RawM3UString = 'catchup="append" catchup-minutes="' .. archive_minutes .. '" catchup-source=""'
+				if tab.data[i].attributes
+					and tab.data[i].attributes.streams
+					and tab.data[i].attributes.streams[1]
+					and tab.data[i].attributes.streams[1].id
+				then
+					t[#t + 1] = {}
+					t[#t].name = tab.data[i].attributes.name
+					t[#t].address = 'https://limehd.tv/' .. tab.data[i].attributes.streams[1].id
+					t[#t].logo = tab.data[i].attributes.image_url
+					local archive_minutes = (tab.data[i].attributes.streams[1].archive_hours or 0) * 60
+					t[#t].RawM3UString = 'catchup="append" catchup-minutes="' .. archive_minutes .. '"'
+				end
 				i = i + 1
 			end
 	 return t
