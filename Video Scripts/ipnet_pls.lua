016e6a0f6382d4828cdd5d6fc5597572f1e48d91
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-08 19:45:57 +0300

    ipnet

---
+++ Video Scripts/ipnet_pls.lua
@@ -0,0 +1,75 @@
+-- скрапер TVS для загрузки плейлиста "Ipnet" https://ipnet.ua (8/1/23)
+-- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- ## переименовать каналы ##
+local filter = {
+	{'ЛДПР LIVE', 'ЛДПР ТВ'},
+	{'Липецкое время', 'Липецкое время (Липецк)'},
+	}
+	module('ipnet_pls', package.seeall)
+	local my_src_name = 'Ipnet'
+	local function ProcessFilterTableLocal(t)
+		if not type(t) == 'table' then return end
+		for i = 1, #t do
+			t[i].name = tvs_core.tvs_clear_double_space(t[i].name)
+			for _, ff in ipairs(filter) do
+				if (type(ff) == 'table' and t[i].name == ff[1]) then
+					t[i].name = ff[2]
+				end
+			end
+		end
+	 return t
+	end
+	function GetSettings()
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\ipnet.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, show_progress = 0, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1, TypeFindUseGr	=0}}
+	end
+	function GetVersion()
+	 return 2, 'UTF-8'
+	end
+	local function LoadFromSite()
+		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
+			if not session then return end
+		m_simpleTV.Http.SetTimeout(session, 8000)
+		local url = decode64('aHR0cDovL2FwaS50di5pcG5ldC51YS9hcGkvdjIvc2l0ZS9jaGFubmVscw')
+		local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+			if rc ~= 200 then return end
+		answer = answer:gsub('%[%]', '""')
+		require 'json'
+		local err, tab = pcall(json.decode, answer)
+			if not tab
+				or not tab.data
+				or not tab.data.categories
+				or not tab.data.categories[1]
+				or not tab.data.categories[1].channels
+			then
+			 return
+			end
+		local t, i = {}, 1
+			while tab.data.categories[1].channels[i] do
+				t[#t + 1] = {}
+				t[#t].name = tab.data.categories[1].channels[i].name
+				t[#t].address = tab.data.categories[1].channels[i].url
+				t[#t].logo = tab.data.categories[1].channels[i].icon_url
+				if tab.data.categories[1].channels[i].is_tshift_allowed == true then
+					local archive_minutes = (tab.data.categories[1].channels[i].tshift_duration or 0) / 60
+					t[#t].RawM3UString = 'catchup="append" catchup-minutes="' .. archive_minutes .. ' catchup-source="?timeshift=${start}"'
+				end
+				i = i + 1
+			end
+	 return t
+	end
+	function GetList(UpdateID, m3u_file)
+			if not UpdateID then return end
+			if not m3u_file then return end
+			if not TVSources_var.tmp.source[UpdateID] then return end
+		local Source = TVSources_var.tmp.source[UpdateID]
+		local t_pls = LoadFromSite()
+			if not t_pls or #t_pls == 0 then return end
+		t_pls = ProcessFilterTableLocal(t_pls)
+		local m3ustr = tvs_core.ProcessFilterTable(UpdateID, Source, t_pls)
+		local handle = io.open(m3u_file, 'w+')
+			if not handle then return end
+		handle:write(m3ustr)
+		handle:close()
+	 return 'ok'
+	end
+-- debug_in_file(#t_pls .. '\n')
