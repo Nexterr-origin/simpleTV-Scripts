88aed82e61a086b851e1879f337562e338d58136
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-09 09:16:07 +0300

    Update vinteratv_pls.lua

--- Scrapers TVSources/vinteratv_pls.lua
+++ Scrapers TVSources/vinteratv_pls.lua
@@ -1,18 +1,12 @@
--- скрапер TVS для загрузки плейлиста "Винтера" http://www.vintera.tv (19/5/21)
--- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- скрапер TVS для загрузки плейлиста "Винтера" https://vintera.tv (9/1/23)
+-- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
--- видоскрипт: vintera.lua
+-- видоскрипт: mediavitrina.lua, vinteratv.lua
 -- ## переименовать каналы ##
 local filter = {
 	{'26 Регион', '26 Регион HD (Ставрополь)'},
 	{'Липецкое время', 'Липецкое время (Липецк)'},
-	{'Матч ТВ (500 кб/с)', 'Матч ТВ'},
-	{'НТВ (500 кб/с)', 'НТВ'},
-	{'Первый канал (500 кб/с)', 'Первый канал'},
 	{'РБК.', 'РБК'},
-	{'РЕН ТВ (500 кб/с)', 'РЕН ТВ'},
-	{'Россия 1 (500 кб/с)', 'Россия 1'},
-	{'Россия 24 (500 кб/с)', 'Россия 24'},
 	{'ТНТ (500 кб/с)', 'ТНТ'},
 	{'СЕВЕР', 'Север (Нарьян-Мар)'},
 	{'Юрган', 'Юрган (Сыктывкар)'},
@@ -39,7 +33,6 @@ local filter = {
 	{'Телеканал С-1', 'С1 (Сургут)'},
 	{'Психология ТВ', 'Психология 21'},
 	}
--- ##
 	module('vinteratv_pls', package.seeall)
 	local my_src_name = 'Винтера'
 	local function ProcessFilterTableLocal(t)
@@ -55,44 +48,87 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\vinteratv.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\vinteratv.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, show_progress = 0, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1, TypeFindUseGr = 0}}
 	end
 	function GetVersion()
 	 return 2, 'UTF-8'
 	end
-	local function showMsg(str, color)
-		local t = {text = str, color = color, showTime = 1000 * 10, id = 'channelName'}
-		m_simpleTV.OSD.ShowMessageT(t)
-	end
-	function LoadFromSite()
-		local session = m_simpleTV.Http.New('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 4 Build/LMY48T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.89 Mobile Safari/537.36')
+	local function LoadFromSite()
+		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
-		htmlEntities = require 'htmlEntities'
-		local urlsTab ={'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvcHJvdnR2LnhtbD9sYW5nPXJ1', 'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvcHJlbWl1bS9wYWNrYWdlc19ydS54bWw', 'aHR0cDovL3htbC52aW50ZXJhLnR2L2FuZHJvaWRfdjExMTcvaW50ZXJuZXR0di54bWw/bGFuZz1ydXM'}
-		local t = {}
-			for i = 1, #urlsTab do
-				local rc, answer = m_simpleTV.Http.Request(session, {url = decode64(urlsTab[i])})
-				if rc == 200 then
-					for w in answer:gmatch('<track(.-)</track>') do
-						local adr = w:match('<location>([^<]+)')
-						local title = w:match('<title>([^<]+)')
-							if adr and adr:match('%.m3u8') and title then
-								t[#t + 1] = {}
-								t[#t].name = htmlEntities.decode(title)
-								t[#t].address = adr:gsub('&amp;', '&') .. '&tvin'
-							end
+		require 'json'
+			local function GetTab(url)
+				url = decode64(url)
+				local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+					if rc ~= 200 then return end
+				answer = answer:gsub('\\', '\\\\')
+				answer = answer:gsub('\\"', '\\\\"')
+				answer = answer:gsub('\\/', '/')
+				answer = answer:gsub('%[%]', '""')
+				answer = unescape3(answer)
+				local err, tab = pcall(json.decode, answer)
+				local t = {}
+				if url:match('premium') then
+						if not tab
+							or not tab.package
+						then
+						 return
+						end
+					local j = 1
+						while tab.package[j] do
+							local i = 1
+								while tab.package[j].trackList.track[i] do
+							t[#t + 1] = {}
+							t[#t].name = tab.package[j].trackList.track[i].title
+							t[#t].address = 'https://www.vinteratv.com/?channel=' .. tab.package[j].trackList.track[i].id
+							t[#t].logo = tab.package[j].trackList.track[i].image
+							i = i + 1
+						end
+						j = j + 1
 					end
+				else
+						if not tab
+							or not tab.trackList
+							or not tab.trackList.track
+						then
+						 return
+						end
+					local i = 1
+						while tab.trackList.track[i] do
+							t[#t + 1] = {}
+							t[#t].name = tab.trackList.track[i].title
+							t[#t].address = 'https://www.vinteratv.com/?channel=' ..tab.trackList.track[i].id
+							t[#t].logo = tab.trackList.track[i].image
+							i = i + 1
+						end
+					end
+			 return t
+			end
+			local function tables_concat(t1, t2)
+				local t3 = {unpack(t1)}
+				local p = #t3
+					for i = 1, #t2 do
+						p = p + 1
+						t3[p] = t2[i]
+					end
+			 return t3
+			end
+		local tab1 = GetTab('aHR0cHM6Ly94bWwudmludGVyYS50di93aWRnZXRfYXBpL2ludGVybmV0dHYueG1sP2Zvcm1hdD1qc29uJmxhbmc9cnU') or {}
+		local tab2 = GetTab('aHR0cHM6Ly94bWwudmludGVyYS50di93aWRnZXRfYXBpL3Byb3Z0di54bWw/Zm9ybWF0PWpzb24mbGFuZz1ydQ') or {}
+		local tab3 = GetTab('aHR0cHM6Ly94bWwudmludGVyYS50di93aWRnZXRfYXBpL3ByZW1pdW0vcGFja2FnZXNfcnUueG1sP2Zvcm1hdD1qc29uJmxhbmc9cnU') or {}
+		local tab = tables_concat(tab1, tab2)
+		tab = tables_concat(tab, tab3)
+		m_simpleTV.Http.Close(session)
+			if #tab == 0 then return end
+		local hash, t = {}, {}
+			for _, v in ipairs(tab) do
+				if not hash[v.name] then
+					t[#t + 1] = v
+					hash[v.name] = true
 				end
 			end
-		local hash, t0 = {}, {}
-			for _, v in ipairs(t) do
-				if not hash[v.address] then
-					t0[#t0 + 1] = v
-					hash[v.address] = true
-				end
-			end
-	 return t0
+	 return t
 	end
 	function GetList(UpdateID, m3u_file)
 			if not UpdateID then return end
@@ -100,12 +136,8 @@ local filter = {
 			if not TVSources_var.tmp.source[UpdateID] then return end
 		local Source = TVSources_var.tmp.source[UpdateID]
 		local t_pls = LoadFromSite()
-			if not t_pls then
-				showMsg(Source.name .. ' ошибка загрузки плейлиста', ARGB(255, 255, 102, 0))
-			 return
-			end
+			if not t_pls or #t_pls == 0 then return end
 		t_pls = ProcessFilterTableLocal(t_pls)
-		showMsg(Source.name .. ' (' .. #t_pls .. ')', ARGB(255, 153, 255, 153))
 		local m3ustr = tvs_core.ProcessFilterTable(UpdateID, Source, t_pls)
 		local handle = io.open(m3u_file, 'w+')
 			if not handle then return end
