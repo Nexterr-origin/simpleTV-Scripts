5a986e5237cfe32a690f9ce0f93bc9b3b01b5b8a
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-11 13:38:13 +0300

    Update ipnet_pls.lua

--- Scrapers TVSources/ipnet_pls.lua
+++ Scrapers TVSources/ipnet_pls.lua
@@ -1,4 +1,4 @@
--- скрапер TVS для загрузки плейлиста "Ipnet" https://tv.ipnet.ua (10/1/23)
+-- скрапер TVS для загрузки плейлиста "Ipnet" https://tv.ipnet.ua (11/1/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ## (если гео-блок)
 -- видоскрипт: ipnet.lua
@@ -21,7 +21,7 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\ipnet.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, show_progress = 0, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1, TypeFindUseGr = 0}}
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\ipnet.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, show_progress = 0, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 1, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1, TypeFindUseGr = 1}}
 	end
 	function GetVersion()
 	 return 2, 'UTF-8'
@@ -44,15 +44,19 @@ local filter = {
 			then
 			 return
 			end
+		require 'lfs'
+		local videoScript = lfs.attributes(m_simpleTV.MainScriptDir_UTF8 .. '/user/video/ipnet.lua', 'size')
 		local t, i = {}, 1
 			while tab.data.categories[1].channels[i] do
 				t[#t + 1] = {}
 				t[#t].name = tab.data.categories[1].channels[i].name
 				t[#t].address = tab.data.categories[1].channels[i].url
 				t[#t].logo = tab.data.categories[1].channels[i].icon_url
-				if tab.data.categories[1].channels[i].is_tshift_allowed == true then
-					local archive_minutes = (tab.data.categories[1].channels[i].tshift_duration or 0) / 60
-					t[#t].RawM3UString = 'catchup="append" catchup-minutes="' .. archive_minutes .. ' catchup-source="?timeshift=${start}"'
+				if not videoScript then
+					if tab.data.categories[1].channels[i].is_tshift_allowed == true then
+						local archive_minutes = (tab.data.categories[1].channels[i].tshift_duration or 0) / 60
+						t[#t].RawM3UString = 'catchup="append" catchup-minutes="' .. archive_minutes .. ' catchup-source="?timeshift=${start}"'
+					end
 				end
 				i = i + 1
 			end
