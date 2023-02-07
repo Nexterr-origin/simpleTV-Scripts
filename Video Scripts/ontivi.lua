c66831c9fe9568cbef298e42ced6a683c1cfaad7
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-02-08 01:58:27 +0300

    Update ontivi.lua

--- Video Scripts/ontivi.lua
+++ Video Scripts/ontivi.lua
@@ -1,5 +1,5 @@
--- видеоскрипт для плейлиста "ontivi" http://ontivi.net (9/7/22)
--- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для плейлиста "ontivi" http://ontivi.net (8/2/23)
+-- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- модуль: /core/playerjs.lua
 -- скрапер TVS: ontivi_pls.lua
@@ -7,42 +7,39 @@
 -- http://ip.ontivi.net/domkinotv.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://[%w%.]*ontivi%.net') then return end
-	local inAdr = m_simpleTV.Control.CurrentAddress
 	require 'playerjs'
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
+	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0'
-	local session = m_simpleTV.Http.New(userAgent)
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 		if rc ~= 200 then return end
 	local host = inAdr:match('https?://.-/')
-	answer = answer:gsub('<!%-%-.-%-%->', ''):gsub('/%*.-%*/', '')
+	answer = answer:gsub('<![^>]+>', ''):gsub('/%*.-%*/', '')
 	local s = answer:match('%.get%("(open.-",%s*{%a+:\'[^\']+)')
 		if not s then return end
 	s = s:gsub('",%s*{', '?'):gsub(':\'', '=')
 	rc, answer = m_simpleTV.Http.Request(session, {url = host .. s, headers = 'Referer: ' .. inAdr})
-	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	answer = answer:gsub('%s', '')
-	local scr = answer:match('Playerjs%("([^"]+)')
-		if not scr then return end
+	local retAdr = answer:match('file:"([^"]+)')
+		if not retAdr then return end
 	local playerjs_url = answer:match('<scriptsrc="([^"]+)')
 		if not playerjs_url then return end
 	playerjs_url = host .. playerjs_url
-	local retAdr = playerjs.decode(scr, playerjs_url)
-		if not retAdr or retAdr == '' then return end
-	retAdr = retAdr:match('"file":"([^"]+)')
-		if not retAdr then return end
-	local v1= '8?'
-	local v2 = 'Sign='
-	local v3 = 'p'
-	local v4 = answer:match('kan="([^"]+)')	or ''
-	local v5 = answer:match('time="([^"]+)') or ''
-	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3):gsub('{v4}', v4):gsub('{v5}', v5)
+	retAdr = playerjs.decode(retAdr, playerjs_url)
+	if retAdr and not retAdr:match('{%a%d}') then
+		retAdr = playerjs.decode(retAdr, playerjs_url)
+	end
+		if not retAdr or #retAdr == 0 then return end
+	retAdr = retAdr:match('"file":"([^"]+)') or retAdr
+	local v1 = answer:match('varkodk="([^"]+)') or ''
+	local v2 = answer:match('varkos="([^"]+)') or ''
+	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2)
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
