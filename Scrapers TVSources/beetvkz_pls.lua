47f89b5f964dae42adcf75842190af5a79bcd0e6
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-05-27 14:57:24 +0300

    Update beetvkz_pls.lua

--- Scrapers TVSources/beetvkz_pls.lua
+++ Scrapers TVSources/beetvkz_pls.lua
@@ -1,4 +1,4 @@
--- скрапер TVS для загрузки плейлиста "beetvkz" https://beetv.kz (27/5/23)
+-- скрапер TVS для загрузки плейлиста "beetvkz" https://beetv.kz (28/5/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- видоскрипт: beetvkz.lua
@@ -21,7 +21,7 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\okkotv.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\beetvkz.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
 	end
 	function GetVersion()
 	 return 2, 'UTF-8'
@@ -30,7 +30,7 @@ local filter = {
 		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
-		local url = decode64('aHR0cHM6Ly9hcGkuYmVldHYua3ovdjMvY2hhbm5lbHMuanNvbj9jbGllbnRfaWQ9M2UyODY4NWMtZmNlMC00OTk0LTlkM2EtMWRhZDI3NzZlMTZhJmNsaWVudF92ZXJzaW9uPTQuMy4wLTEwMyZsb2NhbGU9cnUtUlUmdGltZXpvbmU9MTA4MDAmZXhwYW5kW2NoYW5uZWxdPWxpdmVfcHJldmlldyxjYXRjaHVwX2F2YWlsYWJpbGl0eSZwYWdlW2xpbWl0XT01MDA')
+		local url = decode64('aHR0cHM6Ly9hcGkuYmVldHYua3ovdjMvY2hhbm5lbHMuanNvbj9jbGllbnRfaWQ9M2UyODY4NWMtZmNlMC00OTk0LTlkM2EtMWRhZDI3NzZlMTZhJmNsaWVudF92ZXJzaW9uPTQuMy4wLTEwMyZsb2NhbGU9cnUtUlUmdGltZXpvbmU9MTA4MDAmZXhwYW5kW2NoYW5uZWxdPWxpdmVfc3RyZWFtLGNhdGNodXBfYXZhaWxhYmlsaXR5JnBhZ2VbbGltaXRdPTUwMA')
 		local rc, answer = m_simpleTV.Http.Request(session, {url = url})
 			if rc ~= 200 then return end
 		answer = answer:gsub('%[%]', '""')
@@ -39,14 +39,10 @@ local filter = {
 			if not tab or not tab.data then return end
 		local t = {}
 			for i = 1, #tab.data do
-				if tab.data[i].live_preview
-					and tab.data[i].live_preview.url_template
-					and tab.data[i].live_preview.url_template:match('/btv/SWM/')
-				then
-					local canal = tab.data[i].live_preview.url_template:match('/btv/SWM/([^/]+)/')
+				if tab.data[i].live_stream then
 					t[#t + 1] = {}
 					t[#t].name = tab.data[i].name
-					t[#t].address = string.format('http://edge01.beetv.kz/btv/SWM/%s/%s.m3u8', canal, canal)
+					t[#t].address = 'https://beetvkz/' .. tab.data[i].live_stream.streaming_uid
 					-- if tab.data[i].catchup_availability
 						-- and tab.data[i].catchup_availability.period
 						-- and tab.data[i].catchup_availability.period.value
