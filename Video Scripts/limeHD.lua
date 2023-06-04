5a0f9651a86616a330275b2d41dd4e0603de0c41
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-06-05 01:09:35 +0300

    Update limeHD.lua

--- Video Scripts/limeHD.lua
+++ Video Scripts/limeHD.lua
@@ -1,12 +1,13 @@
--- видеоскрипт для плейлиста "LimeHD" https://limehd.tv (6/1/23)
+-- видеоскрипт для плейлиста "LimeHD", "LimeHD+" https://limehd.tv (4/6/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
--- скрапер TVS: LimeHD_pls.lua
+-- скрапер TVS: LimeHD_pls.lua, LimeHD+_pls.lua
 -- расширение дополнения httptimeshift: limehd-timeshift_ext.lua
 -- ## открывает подобные ссылки ##
 -- https://limehd.tv/1
+-- https://limehd.tv/channel/match
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://limehd.tv/%d') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://limehd%.tv/') then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
@@ -23,15 +24,35 @@
 	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local id = inAdr:match('%d+')
-	local url = decode64('aHR0cHM6Ly9hcGkuaXB0djIwMjEuY29tL3YxL3N0cmVhbXMv') .. id
-	local headers = decode64('WC1BY2Nlc3MtS2V5OiAxMGFhMDkxMTQ1ODhhNWY3NTBlYWVkNWU5ZGU1MzcwNGM4NThlMTQ0')
-	local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
-		if rc ~= 200 then return end
-	local retAdr = answer:match('"playlist_url":"([^"]+)')
+	local function getStream(inAdr)
+		local id = inAdr:match('%d+')
+			if not id then return end
+		local url = decode64('aHR0cHM6Ly9hcGkuaXB0djIwMjEuY29tL3YxL3N0cmVhbXMv') .. id
+		local headers = decode64('WC1BY2Nlc3MtS2V5OiAxMGFhMDkxMTQ1ODhhNWY3NTBlYWVkNWU5ZGU1MzcwNGM4NThlMTQ0')
+		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
+			if rc ~= 200 then return end
+	 return answer:match('"playlist_url":"([^"]+)'), answer:match('"archive_url":"([^"]+)')
+	end
+	local function getStreamFromSite(inAdr)
+		local channel = inAdr:match('/channel/([^&/%?$]+)')
+			if not channel then return end
+		local url = decode64('aHR0cHM6Ly9saW1laGQudHYvYXBpL3Y0L2NoYW5uZWwv') .. channel .. '?epg=0&tz=3'
+		local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+			if rc ~= 200 then return end
+	 return answer:match('"cdn":"([^"]+)'), answer:match('"archive":"([^"]+)')
+	end
+	local retAdr, url_archive
+	if inAdr:match('/channel/') then
+		retAdr, url_archive = getStreamFromSite(inAdr)
+		if url_archive then
+			url_archive = url_archive:gsub('/$', '')
+		end
+	else
+		retAdr, url_archive = getStream(inAdr)
+	end
+	m_simpleTV.User.limehd.url_archive = 	url_archive
 		if not retAdr then return end
-	m_simpleTV.User.limehd.url_archive = answer:match('"archive_url":"([^"]+)')
-	local extOpt = '$OPT:no-spu$OPT:adaptive-use-avdemux'
+	local extOpt = ''
 	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
 		if rc ~= 200 then return end
 	local t = {}
