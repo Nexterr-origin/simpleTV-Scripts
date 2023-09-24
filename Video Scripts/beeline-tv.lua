b4971acbd1f46745112f2a9d30740ed5ec91ea03
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-09-24 16:54:13 +0300

    beeline-tv.lua

---
+++ Video Scripts/beeline-tv.lua
@@ -0,0 +1,68 @@
+-- видеоскрипт для плейлиста "beeline-tv" https://beeline.tv (11/11/22)
+-- Copyright © 2017-2022 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- ## необходим ##
+-- расширение дополнения httptimeshift: beeline-timesift_ext.lua
+-- ## открывает подобные ссылки ##
+-- https://video.beeline.tv/live/d/channel138.isml/manifest-stb.mpd$OPT:adaptive-use-avdemux$OPT:avdemux-options={decryption_key=b6c1c3ca8245c6447ab75fcab90dead4}
+		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://video%.beeline%.tv/live/') then return end
+	if m_simpleTV.Control.CurrentAddress:match('PARAMS=beeline') then return end
+	if m_simpleTV.Control.MainMode == 0 then
+		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = '', UseLogo = 0, Once = 1})
+	end
+	local inAdr = m_simpleTV.Control.CurrentAddress
+	m_simpleTV.Control.ChangeAddress = 'Yes'
+	m_simpleTV.Control.CurrentAddress = 'error'
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
+		if not session then return end
+	m_simpleTV.Http.SetTimeout(session, 8000)
+	local url = inAdr:gsub('$OPT.+', '')
+	local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+		if rc ~= 200 then return end
+	local extOpt = '$OPT:INT-SCRIPT-PARAMS=beeline$OPT:no-spu'
+	local t = {}
+		for w in answer:gmatch('<Representation[^>]+frameRate[^>]+>') do
+			local bw = w:match('bandwidth="(%d+)')
+			local res = w:match('height="(%d+)')
+			if res and bw then
+				bw = tonumber(bw)
+				bw = math.ceil(bw / 100000) * 100
+				t[#t + 1] = {}
+				t[#t].Id = bw
+				t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
+				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', inAdr, bw, extOpt)
+			end
+		end
+		if #t == 0 then return end
+	table.sort(t, function(a, b) return a.Id < b.Id end)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('beeline_qlty') or 10000)
+	t[#t + 1] = {}
+	t[#t].Id = 10000
+	t[#t].Name = '▫ всегда высокое'
+	t[#t].Address = t[#t - 1].Address
+	t[#t + 1] = {}
+	t[#t].Id = 20000
+	t[#t].Name = '▫ адаптивное'
+	t[#t].Address = inAdr .. extOpt
+	local index = #t
+		for i = 1, #t do
+			if t[i].Id >= lastQuality then
+				index = i
+			 break
+			end
+		end
+	if index > 1 then
+		if t[index].Id > lastQuality then
+			index = index - 1
+		end
+	end
+	if m_simpleTV.Control.MainMode == 0 then
+		t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
+		t.ExtParams = {LuaOnOkFunName = 'beelineSaveQuality'}
+		m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
+	end
+	m_simpleTV.Control.CurrentAddress = t[index].Address
+	function beelineSaveQuality(obj, id)
+		m_simpleTV.Config.SetValue('beeline_qlty', id)
+	end
+-- debug_in_file(t[index].Address .. '\n')
