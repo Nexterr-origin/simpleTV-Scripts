dc74a355a55b76d5079761bd87863d3d0863d883
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-09-30 15:33:04 +0300

    Update inetcom.lua

--- Video Scripts/inetcom.lua
+++ Video Scripts/inetcom.lua
@@ -1,9 +1,9 @@
--- видеоскрипт для плейлиста "Inetcom" https://inetcom.tv (9/1/23)
+-- видеоскрипт для плейлиста "Inetcom" https://inetcom.tv (30/9/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: inetcom_pls.lua
 -- ## открывает подобные ссылки ##
--- http://inetcom.tv/294
+-- https://inetcom.tv/294
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://inetcom.tv/%d') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -12,16 +12,23 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
+	local ua = 'Mozilla/5.0 (Linux; Android 7.1.2; A5010 Build/N2G48H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.158 Mobile Safari/537.36'
 	local function getStream(id)
-		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
+		local session = m_simpleTV.Http.New(ua)
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
+		local headers = 'X-Client-Info: AndroidPhone 50327582\nX-Client-Model: OnePlus A5010\nX-Device: 4\nReferer: http://iptv.inetcom.ru/phone_app_v2/index.html?platform=AndroidPhone&serial=50327582\nX-Requested-With: tv.inetcom.phone2'
 		local url = decode64('aHR0cDovL2FwaTQuaW5ldGNvbS50di9jaGFubmVsL2FsbA')
-		local rc, answer = m_simpleTV.Http.Request(session, {url = url})
+		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
 			if rc ~= 200 then return end
 		require 'json'
 		local err, tab = pcall(json.decode, answer)
-			if not tab then return end
+			if not tab
+				or not tab[1]
+				or not tab[1].id
+			then
+			 return
+			end
 		local stream
 			for i = 1, #tab do
 				if tonumber(id) == tonumber(tab[i].id) then
@@ -34,7 +41,7 @@
 	local id = inAdr:match('%d+')
 	local retAdr = getStream(id)
 		if not retAdr then return end
-	local extOpt = ''
+	local extOpt = '$OPT:http-user-agent=' .. ua
 	retAdr = retAdr .. extOpt
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
