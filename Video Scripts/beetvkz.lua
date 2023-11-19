7578ddfcef3c6754a9be3c19b66874db500fffbe
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-11-19 22:23:27 +0300

    Update beetvkz.lua

--- Video Scripts/beetvkz.lua
+++ Video Scripts/beetvkz.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "beetvkz" https://beetv.kz (1/6/23)
+-- видеоскрипт для плейлиста "beetvkz" https://beetv.kz (19/11/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: beetvkz_pls.lua
@@ -16,6 +16,7 @@
 	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
+	inAdr = inAdr:gsub('^https://', 'http://')
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 		if rc ~= 200 then return end
 	local extOpt = ''
