32206e26de0b1bc67b17fcca76bcd3104d60b6b8
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-11-22 22:39:20 +0300

    Update beetvkz.lua

--- Video Scripts/beetvkz.lua
+++ Video Scripts/beetvkz.lua
@@ -1,24 +1,61 @@
--- видеоскрипт для плейлиста "beetvkz" https://beetv.kz (20/11/23)
+-- видеоскрипт для плейлиста "beetvkz" https://beetv.kz (22/11/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: beetvkz_pls.lua
 -- ## открывает подобные ссылки ##
 -- https://beetvkz/100006592
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://beetvkz/%d') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://beetvkz/%d+') then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress:match('%d+')
-	inAdr = decode64('aHR0cDovL3VjZG4uYmVldHYua3ovYnR2L2xpdmUvaGxzLw') .. inAdr .. '.m3u8'
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0')
+	local userAgent = 'Mozilla/5.0 (iPhone; CPU OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/14E304 Safari/605.1.15'
+	local session = m_simpleTV.Http.New(userAgent, false, true)
 		if not session then return end
-	m_simpleTV.Http.SetTimeout(session, 8000)
-	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
+	m_simpleTV.Http.SetTimeout(session, 20000)
+	if not m_simpleTV.User then
+		m_simpleTV.User = {}
+	end
+	if not m_simpleTV.User.beetvkz then
+		m_simpleTV.User.beetvkz = {}
+	end
+	if not m_simpleTV.User.beetvkz.restart then
+		m_simpleTV.User.beetvkz.restart = 0
+	end
+	inAdr = string.format('%s%s.m3u8?b_app_id=&b_device_platform=windows&b_strmr_channel_id=%s', decode64('aHR0cHM6Ly91Y2RuLmJlZXR2Lmt6L2J0di9saXZlL2hscy8'), inAdr, inAdr)
+	local function GetLocationUrl(url)
+		m_simpleTV.Http.SetRedirectAllow(session, false)
+		local rc = m_simpleTV.Http.Request(session, {url = url, headers = 'Referer: https://beetv.kz/'})
+		local raw = m_simpleTV.Http.GetRawHeader(session)
+		if rc ~= 200 and raw then
+			url = raw:match('Location:%s*(%S+)') or url
+		 return url, rc
+		end
+	 return url, 200
+	end
+	local extOpt = '$OPT:http-user-agent=' .. userAgent
+	local retAdr, rc = GetLocationUrl(inAdr)
+	if rc == - 1 then
+		m_simpleTV.Common.Sleep(1400)
+		retAdr, rc = GetLocationUrl(inAdr)
+	elseif rc ~= 200 then
+		retAdr, rc = GetLocationUrl(retAdr)
+	end
+	if rc ~= 200 then
+		retAdr, rc = GetLocationUrl(retAdr)
+	end
+		if rc == 404 and m_simpleTV.User.beetvkz.restart < 11 then
+			m_simpleTV.User.beetvkz.restart = m_simpleTV.User.beetvkz.restart + 1
+			m_simpleTV.Control.Restart(-2.0, true)
+		 return
+		end
+	m_simpleTV.User.beetvkz.restart = 0
+	retAdr = retAdr:gsub('^https://','http://'):gsub(':443','')
+	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr, headers = 'Referer: https://beetv.kz/'})
 		if rc ~= 200 then return end
-	local extOpt = ''
 	local t = {}
 		for w in answer:gmatch('EXT%-X%-STREAM%-INF(.-)\n') do
 			local bw = w:match('BANDWIDTH=(%d+)')
@@ -29,11 +66,11 @@
 				t[#t + 1] = {}
 				t[#t].Id = bw
 				t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
-				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', inAdr, bw, extOpt)
+				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', retAdr, bw, extOpt)
 			end
 		end
 		if #t == 0 then
-			m_simpleTV.Control.CurrentAddress = inAdr .. extOpt
+			m_simpleTV.Control.CurrentAddress = retAdr .. extOpt
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
@@ -45,7 +82,7 @@
 	t[#t + 1] = {}
 	t[#t].Id = 50000
 	t[#t].Name = '▫ адаптивное'
-	t[#t].Address = inAdr .. extOpt
+	t[#t].Address = retAdr .. extOpt
 	local index = #t
 		for i = 1, #t do
 			if t[i].Id >= lastQuality then
