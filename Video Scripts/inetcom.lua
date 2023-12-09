9c11a90d9091ae274b02895c44e0f628509e20e3
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-12-09 06:18:41 +0300

    Update inetcom.lua

--- Video Scripts/inetcom.lua
+++ Video Scripts/inetcom.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "Inetcom" https://inetcom.tv (30/9/23)
+-- видеоскрипт для плейлиста "Inetcom" https://inetcom.tv (9/12/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: inetcom_pls.lua
@@ -12,9 +12,9 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local ua = 'Mozilla/5.0 (Linux; Android 7.1.2; A5010 Build/N2G48H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.158 Mobile Safari/537.36'
+	local userAgent = 'Mozilla/5.0 (Linux; Android 7.1.2; A5010 Build/N2G48H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.158 Mobile Safari/537.36'
 	local function getStream(id)
-		local session = m_simpleTV.Http.New(ua)
+		local session = m_simpleTV.Http.New(userAgent)
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
 		local headers = decode64('WC1DbGllbnQtSW5mbzogQW5kcm9pZFBob25lIDUwMzI3NTgyClgtQ2xpZW50LU1vZGVsOiBPbmVQbHVzIEE1MDEwClgtRGV2aWNlOiA0ClJlZmVyZXI6IGh0dHA6Ly9pcHR2LmluZXRjb20ucnUvcGhvbmVfYXBwX3YyL2luZGV4Lmh0bWw/cGxhdGZvcm09QW5kcm9pZFBob25lJnNlcmlhbD01MDMyNzU4MgpYLVJlcXVlc3RlZC1XaXRoOiB0di5pbmV0Y29tLnBob25lMg')
@@ -41,7 +41,8 @@
 	local id = inAdr:match('%d+')
 	local retAdr = getStream(id)
 		if not retAdr then return end
-	local extOpt = '$OPT:http-user-agent=' .. ua
+	local extOpt = '$OPT:http-user-agent=' .. userAgent
+	retAdr = retAdr:gsub('//hls%d%.', '//hls1.')
 	retAdr = retAdr .. extOpt
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
