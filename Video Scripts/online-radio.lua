642927a5c34b734762ec407f8b90421ef5dc1ec6
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2024-10-06 18:17:05 +0300

    Update online-radio.lua

--- Video Scripts/online-radio.lua
+++ Video Scripts/online-radio.lua
@@ -1,9 +1,11 @@
--- видеоскрипт для сайта online-radio.su (4/9/24)
+-- видеоскрипт для сайта online-radio.su, onlinetv.su (7/10/24)
 -- Copyright © 2017-2024 Nexterr, NEKTO666 | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## открывает подобные ссылки ##
 -- https://online-radio.su/tv/educational/162-prikljuchenija-hd.html
+-- https://onlinetv.su/tv/sport/211-qazsport.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://online%-radio%.su') 
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://online%-radio%.su')
+			and not m_simpleTV.Control.CurrentAddress:match('^https?://onlinetv%.su')
 		then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
@@ -11,12 +13,11 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0')
 		if not session then return end
-	m_simpleTV.Http.SetTimeout(session, 20000)
+	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 		if rc ~= 200 then return end
-	local retAdr = answer:match('file:"([^"]+)')
-		if not retAdr then return end
+	local retAdr = answer:match('file:"([^"]+)') or answer:match('<source type="video%/mp4" src="([^"]+)')
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
