7059dd913298ef14d03a27d69a5e4ec6fb3a35ee
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2025-08-10 14:30:38 +0300

    Update itvuz.lua

--- Video Scripts/itvuz.lua
+++ Video Scripts/itvuz.lua
@@ -1,7 +1,8 @@
--- видеоскрипт для плейлиста "ITV UZ" https://itv.uz (5/5/25)
+-- видеоскрипт для плейлиста "ITV UZ" https://itv.uz (10/8/25)
 -- Copyright © 2017-2025 Nexterr, NEKTO666 | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: itvuz_pls.lua
+-- расширение дополнения httptimeshift: itvuz-timeshift_ext.lua
 -- ## открывает подобные ссылки ##
 -- https://itv.uz/ru/channels/player/262
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
@@ -11,13 +12,21 @@
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress
+	
+	if not m_simpleTV.User then
+		m_simpleTV.User = {}
+	end
+	if not m_simpleTV.User.itvuz then
+		m_simpleTV.User.itvuz = {}
+	end
+	
 	local id = inAdr:match('([^/]+)$')
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
 	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly9hcGkuaXR2LnV6L3YyL2NhcmRzL2NoYW5uZWxzL3Nob3c/Y2hhbm5lbElkPQ') .. id})
+	local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly9hcGkuaXR2LnV6L3YyL2NhcmRzL2NoYW5uZWxzL3Nob3c/Y2hhbm5lbElkPQ') .. id .. decode64('JnZpZGVvRm9ybWF0PWhscw')})
 		if rc ~= 200 then return end
 	answer = answer:gsub('\\', '\\\\')
 	answer = answer:gsub('\\"', '\\\\"')
@@ -25,9 +34,68 @@
 	answer = answer:gsub('%[%]', '""')
 	local retAdr
 	require 'json'
+	
 	local err, tab = pcall(json.decode, answer)
 		if not tab or not tab.data then return end
 			retAdr = tab.data.files.streamUrl
 		if not retAdr then return end
-	m_simpleTV.Control.CurrentAddress = retAdr
+		
+		m_simpleTV.User.itvuz.url_archive = tab.data.files.timeshiftUrl
+		
+		rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
+		if rc ~= 200 then return end
+	
+		local t = {}
+		for w in string.gmatch(answer, 'EXT%-X%-STREAM%-INF.-m3u8') do
+			local res = w:match('RESOLUTION=%d+x(%d+)')
+			local bw = w:match('BANDWIDTH=(%d+)')
+			local track = w:match('tracks%-v.-%.m3u8')
+			
+			if bw and res then
+				bw = math.ceil(tonumber(bw) / 10000) * 10
+				t[#t + 1] = {}
+				t[#t].Id = bw
+				t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
+				t[#t].Address = retAdr:gsub('index.m3u8', track)
+			end
+		end
+		if #t == 0 then
+			m_simpleTV.Control.CurrentAddress = retAdr
+		 return
+		end
+	table.sort(t, function(a, b) return a.Id < b.Id end)
+	local lastQuality = tonumber(m_simpleTV.Config.GetValue('itvuz_qlty') or 20000)
+	local index = #t
+	if #t > 1 then
+		t[#t + 1] = {}
+		t[#t].Id = 20000
+		t[#t].Name = '▫ всегда высокое'
+		t[#t].Address = t[#t - 1].Address
+		t[#t + 1] = {}
+		t[#t].Id = 50000
+		t[#t].Name = '▫ адаптивное'
+		t[#t].Address = retAdr
+		index = #t
+			for i = 1, #t do
+				if t[i].Id >= lastQuality then
+					index = i
+				 break
+				end
+			end
+		if index > 1 then
+			if t[index].Id > lastQuality then
+				index = index - 1
+			end
+		end
+		if m_simpleTV.Control.MainMode == 0 then
+			t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
+			t.ExtParams = {LuaOnOkFunName = 'itvSaveQuality'}
+			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128 + 8)
+		end
+	end
+	
+	m_simpleTV.Control.CurrentAddress = t[index].Address
+	function itvSaveQuality(obj, id)
+		m_simpleTV.Config.SetValue('itvuz_qlty', id)
+	end
 -- debug_in_file(m_simpleTV.Control.CurrentAddress .. '\n')
